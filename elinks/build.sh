e45ed8fb247d51fd72de41b0f0e6482afe4d6f82
Tom G. Christensen <tgc@jupiterrise.com>
2003-07-30 18:00:38 +0000

    elinks build It was necessary to add bsd-snprint from openssh since elinks depends on snprint being available.

---
+++ elinks/build.sh
@@ -0,0 +1,107 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=elinks
+version=0.4.3rc1
+pkgver=2
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+patch[0]=bsd-snprintf.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="ELinks - WWW browser"
+pkgvendor="http://elinks.or.cz"
+pkgdesc="ELinks is an advanced text-mode web (HTTP/FTP/..) browser"
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    setdir source
+    export CPPFLAGS="-I/usr/local/include/openssl"
+    export LDFLAGS="-L/usr/local/lib -rpath /usr/local/lib"
+    ./configure --prefix=/usr/local --with-openssl=/usr/local/ssl --without-gpm --without-lua --with-x=no
+    $MAKE_PROG
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
