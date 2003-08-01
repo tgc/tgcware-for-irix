4dd8521a326849db15a465fb385bf959133f73a0
Tom G. Christensen <tgc@jupiterrise.com>
2003-08-01 11:21:37 +0000

    libjpeg for Irix

---
+++ libjpeg/build.sh
@@ -0,0 +1,108 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=libjpeg
+version=6b
+pkgver=1
+source[0]=jpegsrc.v6b.tar.gz
+# If there are no patches, simply comment this
+patch[0]=jpeg-c++.patch
+patch[1]=libjpeg-6b-arm.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="libjpeg"
+
+topsrcdir=jpeg-$version
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    export LDFLAGS='-rpath /usr/local/lib'
+    setdir source
+    ./configure --prefix=$prefix --disable-static --enable-shared
+    LD_LIBRARY_PATH=$PWD $MAKE_PROG test
+    $MAKE_PROG
+}
+
+reg install
+install()
+{
+    generic_install prefix
+}
+
+reg pack
+pack()
+{
+    generic_pack shortroot
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
