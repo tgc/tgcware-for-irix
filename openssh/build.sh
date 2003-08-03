b9da5121da76e58e026b3c7b7203e283439569c4
Tom G. Christensen <tgc@jupiterrise.com>
2003-08-03 20:14:12 +0000

    Made the initscript honour verbose and on/off Upped pkg version

--- openssh/build.sh
+++ openssh/build.sh
@@ -9,7 +9,7 @@
 # Check the following 4 variables before running the script
 topdir=openssh
 version=3.6.1p1
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
