208382ec58e41052323ee83559c089e1db969762
Tom G. Christensen <tgc@jupiterrise.com>
2003-08-03 13:03:46 +0000

    mktemp from openbsd ported from the RH RPM package :) The -irix and -mkdtemp patches was inspired by patches from the RPM package modified for use with Irix. The -progname patch is my own hack and since I don't much about C code this was the best I could do.

---
+++ mktemp/build.sh
@@ -0,0 +1,106 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=mktemp
+version=1.5
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+patch[0]=mktemp-1.5-irix.patch
+patch[1]=mktemp-1.5-mkdtemp.patch
+patch[2]=mktemp-1.5-progname.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="mktemp"
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    setdir source
+    $MAKE_PROG
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    setdir stage
+    chown -R tgc:user *
+}
+
+reg pack
+pack()
+{
+    generic_pack shortroot
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
