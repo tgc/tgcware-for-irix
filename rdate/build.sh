42081b664474c7b5de335d436d72172028af30af
Tom G. Christensen <tgc@jupiterrise.com>
2003-12-23 21:48:53 +0000

    rdate - a simple way to syncronize the clock

---
+++ rdate/build.sh
@@ -0,0 +1,105 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=rdate
+version=990821
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="rdate"
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    setdir source
+    $MAKE_PROG CC=gcc CFLAGS=-O2
+}
+
+reg install
+install()
+{
+    setdir source
+    mkdir -p $stagedir$prefix/bin
+    mkdir -p $stagedir$prefix/man/man1
+    $MAKE_PROG BINDIR=$prefix/bin MANDIR=$prefix/man/man1 DESTDIR=$stagedir install
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
