3063c34f9a1658cb7a4690f5f37f4432bd47ed55
Tom G. Christensen <tgc@jupiterrise.com>
2003-12-23 19:50:14 +0000

    Proper linking with -rpath Rebuild with gtk toolkit for gvim

--- vim/build.sh
+++ vim/build.sh
@@ -9,7 +9,7 @@
 # Check the following 4 variables before running the script
 topdir=vim
 version=6.2.55
-pkgver=athena-1
+pkgver=gtk-2
 source[0]=$topdir-6.2.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=6.2.001
@@ -87,8 +87,9 @@ prep()
 reg build
 build()
 {
+    export LDFLAGS="-Wl,-rpath,/usr/local/lib"
     setdir source
-    ./configure --prefix=/usr/local --with-gnome=no --enable-gui=auto --disable-gpm --disable-nls
+    ./configure --prefix=/usr/local --with-gnome=no --enable-gui=gtk --disable-gpm --disable-nls
     $MAKE_PROG
 }
 
