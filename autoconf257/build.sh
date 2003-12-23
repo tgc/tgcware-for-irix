7e526be3324a3cf1c6738de920fc290b635016a5
Tom G. Christensen <tgc@jupiterrise.com>
2003-12-23 15:37:49 +0000

    autoconf 2.57

---
+++ autoconf257/build.sh
@@ -0,0 +1,106 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=autoconf257
+version=2.57
+pkgver=1
+source[0]=autoconf-$version.tar.bz2
+# If there are no patches, simply comment this
+patch[0]=autoconf-2.57-versioning.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="GNU Autoconf"
+
+topsrcdir=autoconf-$version
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    setdir source
+    ./configure --prefix=$prefix --program-suffix=-$version
+    $MAKE_PROG
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    rm -f $stagedir$prefix/info/dir
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
