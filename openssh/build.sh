efd0ede811c3abd397f1015db81e3235e2046ce9
Tom G. Christensen <tgc@jupiterrise.com>
2004-01-01 17:25:37 +0000

    Update to 3.7.1p2 This is the first openssh package to use the new depends, ops and hide files to add dependencies, pre/post scripts and nohist tags to the spec and idb file. Removed the old "fixed" idb and spec files. sshd.init.irix has been changed to use whoami instead of id to determine which use i presently running the script. postinstall.irix was fixed up to not unconditionally add the sshd user to clogin.conf. It now checks for an existing presence. A note about the /var/empty/sshd dir was added to the postremove script.

--- openssh/build.sh
+++ openssh/build.sh
@@ -8,8 +8,8 @@
 #
 # Check the following 4 variables before running the script
 topdir=openssh
-version=3.7.1p1
-pkgver=1
+version=3.7.1p2
+pkgver=5
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -19,10 +19,7 @@ source[0]=$topdir-$version.tar.gz
 
 # Fill in pkginfo values if necessary
 # using pkgname,name,pkgcat,pkgvendor & pkgdesc
-#pkgname=SBossh
 name="OpenSSH"
-pkgvendor="http://www.openssh.org"
-pkgdesc="Secure Shell remote access utility"
 
 # Define script functions and register them
 METHODS=""
@@ -39,7 +36,7 @@ prep()
 reg build
 build()
 {
-    export LDFLAGS="-rpath /usr/local/lib -L/usr/local/lib"
+    export LDFLAGS="-Wl,-rpath,/usr/local/lib -L/usr/local/lib"
     export CPPFLAGS="-I/usr/local/include/openssl"
     setdir source
     ./configure --prefix=$prefix --sysconfdir=$prefix/etc/ssh --with-prngd-socket=/var/run/egd-pool --with-default-path=/usr/local/bin:/usr/bsd:/usr/bin --with-mantype=cat --disable-suid-ssh --without-rsh --with-privsep-user=sshd --with-superuser-path=/usr/sbin:/usr/bsd:/sbin:/usr/bin:/bin:/etc:/usr/etc:/usr/bin/X11:/usr/local/bin
@@ -52,24 +49,19 @@ install()
     clean stage
     setdir source
     $MAKE_PROG DESTDIR=$stagedir install-nokeys
-    strip
     setdir $stagedir$prefix/etc/ssh
     for i in *; do mv $i $i.default; done
     cp -p $metadir/sshd.init.irix $stagedir/usr/local/etc/ssh/sshd.init-sample
     cp -p $metadir/postinstall.irix $stagedir$prefix/etc/ssh/postinstall.irix
     cp -p $metadir/postremove.irix $stagedir$prefix/etc/ssh/postremove.irix
+    chmod 744 $stagedir$prefix/etc/ssh/postremove.irix
+    chmod 744 $stagedir$prefix/etc/ssh/postinstall.irix
 }
 
 reg pack
 pack()
 {
-    clean meta
-    setdir $stagedir$prefix
-    create_idb
-    cp $metadir/$topdir.idb.fixed $metadir/$topdir.idb
-    create_spec
-    cp $metadir/$topdir.spec.fixed $metadir/$topdir.spec
-    make_dist
+    generic_pack
 }
 
 reg distclean
@@ -88,7 +80,7 @@ all()
     for METHOD in $METHODS 
     do
 	case $METHOD in
-	     all*) ;;
+	     all*|*clean) ;;
 	     *) $METHOD
 		;;
 	esac
