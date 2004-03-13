995ea9779e4753fa8c61b70db6f55c24ffd7b748
Tom G. Christensen <tgc@jupiterrise.com>
2004-03-13 17:03:43 +0000

    Automake 1.7.8 from Fedora Core 1 It's setup to be the default automake/aclocal

---
+++ automake17/build.sh
@@ -0,0 +1,112 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=automake17
+version=1.7.8
+pkgver=1
+source[0]=automake-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Fill in pkginfo values if necessary
+# using pkgname,name,pkgcat,pkgvendor & pkgdesc
+name="GNU Automake"
+
+topsrcdir=automake-$version
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    # needs autoconf 2.54+
+    (cd $HOME/bin; ln -s /usr/local/bin/autoconf-2.57 autoconf)
+    setdir source
+    ./configure --prefix=/usr/local --disable-nls
+    $MAKE_PROG
+    (cd $HOME/bin; rm -f autoconf)
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    $RM -f $stagedir$prefix/info/dir
+    gzip -9 $stagedir$prefix/info/automake.info*
+    setdir source
+    doc AUTHORS COPYING ChangeLog INSTALL NEWS README THANKS TODO
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
