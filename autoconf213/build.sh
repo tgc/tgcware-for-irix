55ef943829e69f186b7d786af2a4f876dede7ae3
Tom G. Christensen <tgc@jupiterrise.com>
2004-03-13 17:59:07 +0000

    Included all the patches uses in the Fedora Core 1 rpm release autoconf-2.12-race.patch is disabled for now since it would introduce a subtle requirement on mktemp

--- autoconf213/build.sh
+++ autoconf213/build.sh
@@ -9,10 +9,19 @@
 # Check the following 4 variables before running the script
 topdir=autoconf213
 version=2.13
-pkgver=1
+pkgver=2
 source[0]=autoconf-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=   #autoconf-2.12-race.patch - use /bin/mktemp
+patch[1]=autoconf-2.13-mawk.patch
+patch[2]=autoconf-2.13-notmp.patch
+patch[3]=autoconf-2.13-c++exit.patch
+patch[4]=autoconf-2.13-headers.patch
+patch[5]=autoconf-2.13-autoscan.patch
+patch[6]=autoconf-2.13-exit.patch
+patch[7]=autoconf-2.13-wait3test.patch
+patch[8]=autoconf-2.13-make-defs-62361.patch
+patch[9]=autoconf-2.13-versioning.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -38,19 +47,27 @@ prep()
 reg build
 build()
 {
-    generic_build
+    setdir source
+    ./configure --prefix=$prefix --program-suffix=-$version --disable-nls
+    $MAKE_PROG
 }
 
 reg install
 install()
 {
     generic_install prefix
+    # standards*.info belongs with binutils...
+    $RM -f $stagedir/info/standards*
+    $MV $stagedir/info/autoconf.info $stagedir/info/$topdir.info
+    $GZIP -9nf $stagedir/info/*.info
+    doc AUTHORS COPYING NEWS README TODO
 }
 
 reg pack
 pack()
 {
-    generic_pack shortroot
+    shortroot=1
+    generic_pack
 }
 
 reg distclean
