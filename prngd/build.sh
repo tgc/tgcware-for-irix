ac6f665af01970b402c2a0945c30c1d0e432499e
Tom G. Christensen <tgc@jupiterrise.com>
2004-08-14 10:19:59 +0000

    Irix 5.3 chmod doesn't understand user:group syntax, change to user.group instead in prngd init script

--- prngd/build.sh
+++ prngd/build.sh
@@ -9,7 +9,7 @@
 # Check the following 4 variables before running the script
 topdir=prngd
 version=0.9.29
-pkgver=3
+pkgver=4
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=prngd-irix53-support.patch
