6342f0932b34b827822b7020c0100f42fdf0e410
Tom G. Christensen <tgc@jupiterrise.com>
2004-10-15 19:48:31 +0000

    Attempt to solve the problem of programs needing various versions of libstdc++ and libgcc_s.so.1

---
+++ gccrt/build.sh
@@ -0,0 +1,101 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# The concept of "method" registering and the logic that implements it was shamelessly
+# stolen from jhlj's Compile.sh script :)
+#
+# Check the following 4 variables before running the script
+topdir=gccrt
+version=1.0
+pkgver=1
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+source[0]=$srcdir/$topdir-$version.tar.bz2
+
+# Define script functions and register them
+METHODS=""
+reg() {
+    METHODS="$METHODS $1"
+}
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    echo "Nothing needs to be built"
+}
+
+reg install
+install()
+{
+    clean stage
+    ${MKDIR} -p ${stagedir}/${_libdir}
+    (cd ${srcdir}/${topsrcdir}; ${TAR} -cf - .)|(cd ${stagedir}/${_libdir}; tar -xvBpf -) 
+}
+
+reg pack
+pack()
+{
+    shortroot=1
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+
+reg all
+all()
+{
+    for METHOD in $METHODS 
+    do
+	case $METHOD in
+	     all*|*clean) ;;
+	     *) $METHOD
+		;;
+	esac
+    done
+
+}
+
+reg
+usage() {
+    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
+    exit 1
+}
+
+OK=0
+for METHOD in $*
+do
+    METHOD=" $METHOD *"
+    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
+	usage
+    fi
+    OK=1
+done
+
+if [ $OK = 0 ] ; then
+    usage;
+fi
+
+for METHOD in $*
+do
+    ( $METHOD )
+done
