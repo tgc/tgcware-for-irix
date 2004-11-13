6c7bec105c0bd50c2f29f2b593b1fb52be932e9a
Tom G. Christensen <tgc@jupiterrise.com>
2004-11-13 20:41:57 +0000

    New upstream release. build.sh was cleaned up and meta data updated

--- gcc3/build.sh
+++ gcc3/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bash/bin/bash
+#!/usr/local/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -8,21 +8,21 @@
 #
 # Check the following 4 variables before running the script
 topdir=gcc
-version=3.4.1
-pkgver=3
+version=3.4.3
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+patch[0]=gcc-3.4.3-genfixes.patch
+patch[1]=gcc-3.4.3-iconv.patch
+patch[2]=gcc-3.4.3-iconv-rpath.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# If there are no patches, simply comment this
-#patch[0]=
-
 # Define abbreviated version number
 abbrev_ver=$(echo $version|$SED -e 's/\.//g')
 
-topinstalldir=/usr/local/$topdir-$version
-prefix=$topinstalldir
+prefix=/usr/local/$topdir-$version
 
 # Define script functions and register them
 METHODS=""
@@ -34,40 +34,50 @@ reg prep
 prep()
 {
     generic_prep
+    # Regenerate configure to include libiconv fixes
+    setdir source
+    cd gcc
+    autoreconf # 2.59
+    cd ../libjava
+    autoreconf # 2.59
 }
 
 reg build
 build()
 {
-    # Default to mips3, n32 ABI. No other ABI's available (--disable-multilib turns off o32 & n64)
-    configure_args="--prefix=$prefix --enable-languages=c,c++ --disable-nls --disable-multilib"
+    global_config_args="--prefix=$prefix --with-local-prefix=$prefix --enable-languages=c,c++ --disable-nls --disable-shared --with-libiconv-prefix=/usr/local"
+    if [ "$_os" = "irix53" ]; then
+	export CONFIG_SHELL=/bin/ksh
+	configure_args="$global_config_args"
+    fi
+    if [ "$_os" = "irix62" ]; then
+	configure_args="$global_config_args --disable-multilib"
+	export CC='/usr/people/tgc/bin/cc -n32 -mips3'
+    fi
+    # fix up releasenotes
+    set_configure_args "$configure_args"
     setdir $srcdir
     mkdir -p objdir
     setdir $srcdir/objdir
     $srcdir/$topsrcdir/configure $configure_args
-    $MAKE_PROG bootstrap
-    # Horrible horrible hack for Irix 6.2...
-    echo "#undef MN_NAME_PAT" > $srcdir/objdir/gcc/fixinc/machname.h
     $MAKE_PROG bootstrap
 }
 
 reg install
 install()
 {
+    clean stage
     lprefix=/usr/local
     setdir $srcdir/objdir
-    $MAKE_PROG DESTDIR=$stagedir install
-    $MKDIR ${stagedir}${lprefix}/${_libdir}
-    $CP ${stagedir}${lprefix}/gcc-$version/$_libdir/*.so.* ${stagedir}${lprefix}/${_libdir}
-    setdir ${stagedir}${lprefix}/${_libdir}
-    $LN -sf libstdc++.so.7.1 libstdc++.so.7
+    $MAKE_PROG INSTALL=$GINSTALL DESTDIR=$stagedir install
     $RM -f ${stagedir}${prefix}/${_infodir}/dir
+    custom_install=1
+    generic_install
 }
 
 reg pack
 pack()
 {
-    topinstalldir=/usr/local
     iprefix=$topdir-$version
     generic_pack
 }
@@ -76,6 +86,8 @@ reg distclean
 distclean()
 {
     clean distclean
+    setdir $srcdir
+    ${RM} -rf objdir
 }
 
 ###################################################
