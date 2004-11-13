344b1b83a17f4c69fc09e0b94bb69181650972d6
Tom G. Christensen <tgc@jupiterrise.com>
2004-11-13 20:46:18 +0000

    Rebuild with gcc 3.4.3

--- libiconv/build.sh
+++ libiconv/build.sh
@@ -9,7 +9,7 @@
 # Check the following 4 variables before running the script
 topdir=libiconv
 version=1.9.2
-pkgver=3
+pkgver=4
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
