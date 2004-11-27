c2217b8ce75700b82769c82c0746d16c529fbee7
Tom G. Christensen <tgc@jupiterrise.com>
2004-11-27 12:28:53 +0000

    Remove depend on gccrt

--- bzip2/build.sh
+++ bzip2/build.sh
@@ -9,15 +9,13 @@
 # Check the following 4 variables before running the script
 topdir=bzip2
 version=1.0.2
-pkgver=7
+pkgver=8
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-shortroot=1 # Shallow stagedir
 
 # Define script functions and register them
 METHODS=""
@@ -44,24 +42,24 @@ install()
 {
     clean stage
     setdir source
-    ${MKDIR} -p ${stagedir}/{${_bindir},${_mandir}/man1,${_libdir},${_includedir}}
-    ${GINSTALL} -m 755 bzlib.h ${stagedir}/${_includedir}
-    ${GINSTALL} -m 755 libbz2.so.1.0.2 ${stagedir}/${_libdir}
-    ${GINSTALL} -m 755 libbz2.a ${stagedir}/${_libdir}
-    ${GINSTALL} -m 755 bzip2-shared  ${stagedir}/${_bindir}/bzip2
-    ${GINSTALL} -m 755 bzip2recover bzgrep bzdiff bzmore ${stagedir}/${_bindir}/
-    ${GINSTALL} -m 644 bzip2.1 bzdiff.1 bzgrep.1 bzmore.1 ${stagedir}/${_mandir}/man1/
-    ${LN} -s bzip2 ${stagedir}/${_bindir}/bunzip2
-    ${LN} -s bzip2 ${stagedir}/${_bindir}/bzcat
-    ${LN} -s bzdiff ${stagedir}/${_bindir}/bzcmp
-    ${LN} -s bzmore ${stagedir}/${_bindir}/bzless
-    ${LN} -s libbz2.so.1.0.2 ${stagedir}/${_libdir}/libbz2.so.1.0
-    ${LN} -s libbz2.so.1.0 ${stagedir}/${_libdir}/libbz2.so
-    ${LN} -s bzip2.1 ${stagedir}/${_mandir}/man1/bzip2recover.1
-    ${LN} -s bzip2.1 ${stagedir}/${_mandir}/man1/bunzip2.1
-    ${LN} -s bzip2.1 ${stagedir}/${_mandir}/man1/bzcat.1
-    ${LN} -s bzdiff.1 ${stagedir}/${_mandir}/man1/bzcmp.1
-    ${LN} -s bzmore.1 ${stagedir}/${_mandir}/man1/bzless.1
+    ${MKDIR} -p ${stagedir}${prefix}/{${_bindir},${_mandir}/man1,${_libdir},${_includedir}}
+    ${GINSTALL} -m 755 bzlib.h ${stagedir}${prefix}/${_includedir}
+    ${GINSTALL} -m 755 libbz2.so.1.0.2 ${stagedir}${prefix}/${_libdir}
+    ${GINSTALL} -m 755 libbz2.a ${stagedir}${prefix}/${_libdir}
+    ${GINSTALL} -m 755 bzip2-shared  ${stagedir}${prefix}/${_bindir}/bzip2
+    ${GINSTALL} -m 755 bzip2recover bzgrep bzdiff bzmore ${stagedir}${prefix}/${_bindir}/
+    ${GINSTALL} -m 644 bzip2.1 bzdiff.1 bzgrep.1 bzmore.1 ${stagedir}${prefix}/${_mandir}/man1/
+    ${LN} -s bzip2 ${stagedir}${prefix}/${_bindir}/bunzip2
+    ${LN} -s bzip2 ${stagedir}${prefix}/${_bindir}/bzcat
+    ${LN} -s bzdiff ${stagedir}${prefix}/${_bindir}/bzcmp
+    ${LN} -s bzmore ${stagedir}${prefix}/${_bindir}/bzless
+    ${LN} -s libbz2.so.1.0.2 ${stagedir}${prefix}/${_libdir}/libbz2.so.1.0
+    ${LN} -s libbz2.so.1.0 ${stagedir}${prefix}/${_libdir}/libbz2.so
+    ${LN} -s bzip2.1 ${stagedir}${prefix}/${_mandir}/man1/bzip2recover.1
+    ${LN} -s bzip2.1 ${stagedir}${prefix}/${_mandir}/man1/bunzip2.1
+    ${LN} -s bzip2.1 ${stagedir}${prefix}/${_mandir}/man1/bzcat.1
+    ${LN} -s bzdiff.1 ${stagedir}${prefix}/${_mandir}/man1/bzcmp.1
+    ${LN} -s bzmore.1 ${stagedir}${prefix}/${_mandir}/man1/bzless.1
 
     doc LICENSE CHANGES README README.COMPILATION.PROBLEMS Y2K_INFO
     custom_install=1
