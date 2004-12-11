484c916f6f857ca8fb7c26bc2d376a83d586c8fd
Tom G. Christensen <tgc@jupiterrise.com>
2004-12-11 11:58:41 +0000

    Update to newstyle pkg Update -misc patch from FC2

--- xsnow/build.sh
+++ xsnow/build.sh
@@ -9,7 +9,7 @@
 # Check the following 4 variables before running the script
 topdir=xsnow
 version=1.42
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=xsnow-1.42-misc.patch
@@ -17,10 +17,6 @@ patch[1]=xsnow-1.42-snprintf.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="xsnow"
 
 # Define script functions and register them
 METHODS=""
@@ -44,17 +40,19 @@ build()
 	cp ../$i .
     done
     xmkmf -a
-    $MAKE_PROG CC=gcc CCOPTIONS="-I/usr/local/include" XPMLIB="-L/usr/local/lib -lXpm" LDPOSTLIB= STD_INCLUDES=
+    $MAKE_PROG CC=gcc CCOPTIONS="-I/usr/local/include" SYS_LIBRARIES="-lm -Wl,-rpath,/usr/local/lib -L/usr/local/lib -lXpm" LDPOSTLIB= STD_INCLUDES=
 }
 
 reg install
 install()
 {
-    mkdir -p $stagedir$prefix/bin
-    mkdir -p $stagedir$prefix/man/man1
+    clean stage
+    mkdir -p ${stagedir}${prefix}/${_bindir}
+    mkdir -p ${stagedir}${prefix}/${_mandir}/man1
     setdir source
-    cp xsnow $stagedir$prefix/bin
-    cp xsnow.man $stagedir$prefix/man/man1/xsnow.1
+    cp xsnow ${stagedir}${prefix}/${_bindir}
+    cp xsnow.man ${stagedir}${prefix}/${_mandir}/man1/xsnow.1
+    doc README
 }
 
 reg pack
