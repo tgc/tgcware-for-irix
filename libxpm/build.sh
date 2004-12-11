90f5beb64a65189ec04bb9a0ede48074d9ba2458
Tom G. Christensen <tgc@jupiterrise.com>
2004-12-11 10:47:24 +0000

    Update to newstyle pkg. Fix CAN-2004-687 & CAN-2004-688 (patches from NetBSD pkgsrc) Make sure sxpm links against the new libXpm and not a system provided one.

--- libxpm/build.sh
+++ libxpm/build.sh
@@ -7,26 +7,20 @@
 # stolen from jhlj's Compile.sh script :)
 #
 # Check the following 4 variables before running the script
-topdir=libxpm
+topdir=xpm
 version=3.4k
-pkgver=3
+pkgver=4
 source[0]=xpm-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=libxpm-3.4k-shlib.patch
 patch[1]=libxpm-3.4k-sxpm.patch
 patch[2]=libxpm-3.4k-cxpm.patch
+patch[3]=xpm-3.4k-security-fixes.patch
+patch[4]=xpm-3.4k-size-max.patch
+patch[5]=libxpm-3.4k-sxpm-shlibs.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="XPM library"
-pkgcat="library"
-pkgvendor="http://www.x.org"
-pkgdesc="Store/retrieve X pixmaps to/from files"
-
-topsrcdir=xpm-$version
 
 # Define script functions and register them
 METHODS=""
@@ -44,18 +38,20 @@ reg build
 build()
 {
     setdir source
-    $MAKE_PROG -f Makefile.noX
+    $MAKE_PROG -f Makefile.noX DEFINES="-DNEEDS_SIZE_MAX"
 }
 
 reg install
 install()
 {
+    clean stage
     setdir source
     $MAKE_PROG INSTALL=/usr/local/bin/install DESTDIR=$stagedir -f Makefile.noX install
     $MAKE_PROG INSTALL=/usr/local/bin/install DESTDIR=$stagedir -f Makefile.noX install.man
     setdir $stagedir$prefix/lib
     ln -s libXpm.so.4.11 libXpm.so.4
     ln -s libXpm.so.4.11 libXpm.so
+    doc doc/xpm.PS.gz FAQ.html README.html
 }
 
 reg pack
