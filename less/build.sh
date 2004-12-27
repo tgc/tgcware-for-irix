94a3d39a6c15efcc3206cd21ce23df5ca4378cfe
Tom G. Christensen <tgc@jupiterrise.com>
2004-12-27 19:53:46 +0000

    Less the pager Added lesspipe.sh from Fedora Core 2

---
+++ less/build.sh
@@ -0,0 +1,70 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=less
+version=382
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+source[1]=lesspipe.sh
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/local/include"
+export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
+set_configure_args "--prefix=$prefix --with-editor=/bin/vi"
+# Don't build with ncurses
+export ac_cv_lib_ncurses_initscr=no
+
+shortroot=1
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    setdir source
+    ./configure $configure_args
+    ${SED} -e '/^LIBS/ s/LIBS =.*/LIBS = -lcurses/g' Makefile > Makefile.new
+    ${MV} Makefile.new Makefile
+    $MAKE_PROG
+}
+
+reg install
+install()
+{
+    generic_install prefix
+    ${GINSTALL} -m 755 ${srcdir}/${source[1]} ${stagedir}${prefix}/${_bindir}
+    doc NEWS LICENSE
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
