a4c2289af8f058415a82a63e23aa13a28180d560
Tom G. Christensen <tgc@jupiterrise.com>
2004-12-31 12:21:08 +0000

    Update to latest upstream version Update to newstyle pkg

--- libtiff/build.sh
+++ libtiff/build.sh
@@ -7,24 +7,20 @@
 # stolen from jhlj's Compile.sh script :)
 #
 # Check the following 4 variables before running the script
-topdir=libtiff
-version=3.5.7
-pkgver=3
-source[0]=tiff-v$version.tar.gz
+topdir=tiff
+version=3.7.1
+pkgver=1
+source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=libtiff-v3.5.5-buildroot.patch
-patch[1]=tiff-v3.5-shlib.patch
-patch[2]=libtiff-v3.5.4-codecs.patch
-patch[3]=libtiff-v3.5.7-include.patch
-patch[4]=libtiff-v3.5.7-exit.patch
-patch[5]=libtiff-v3.5.7-shlib.patch
+patch[0]=tiff-3.7.1-cxx.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="Tiff support library"
+# Global settings
+export CPPFLAGS="-I/usr/local/include"
+export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
+set_configure_args '--prefix=$prefix --with-zlib-include-dir=/usr/local/include --with-zlib-lib-dir=/usr/local/lib --with-jpeg-include-dir=/usr/local/include --with-jpeg-lib-dir=/usr/local/lib --disable-cxx'
 
 # Define script functions and register them
 METHODS=""
@@ -32,48 +28,25 @@ reg() {
     METHODS="$METHODS $1"
 }
 
-topsrcdir=tiff-v$version
-subsysconf=$metadir/subsys.conf
-
 reg prep
 prep()
 {
     generic_prep
+    setdir source
+    ./autoconf
 }
 
 reg build
 build()
 {
-    setdir source
-    sed -e "s;@STAGEROOT@;${stagedir};g" port/install.sh.in > port/install.sh.in.new;
-    mv port/install.sh.in.new port/install.sh.in 
-    CDPATH=""
-    unset CDPATH
-    export GCOPTS="-g -rpath $prefix/lib"
-    export INSTALL='${SHELL} ../port/install.sh'
-    ./configure << EOF
-6
-sysv-source-cat
-yes
-EOF
-    (cd libtiff; ln -s libtiff.so.3.5 libtiff.so.3)
-    (cd libtiff; ln -s libtiff.so.3.5 libtiff.so)
-    $MAKE_PROG
-    cp man/*.1 man/apps
+    generic_build
 }
 
 reg install
 install()
 {
-    mkdir -p $stagedir$prefix
-    setdir source
-    $MAKE_PROG install
-    setdir $stagedir$prefix
-    cd lib
-    mv libtiff.so libtiff.so.3.5
-    ln -s libtiff.so.3.5 libtiff.so.3
-    ln -s libtiff.so.3.5 libtiff.so
-    rm -rf ../man/cat1/apps
+    generic_install DESTDIR
+    doc COPYRIGHT
 }
 
 reg pack
