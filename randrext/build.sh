cc29a1f5eb53af5862147d90e9af5f05943ad01f
Tom G. Christensen <tgc@jupiterrise.com>
2005-02-12 10:57:58 +0000

    X side of Xrandr

---
+++ randrext/build.sh
@@ -0,0 +1,57 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=randrext
+version=1.0
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc README COPYING protocol
+    ${RM} -f ${stagedir}${prefix}/${_docdir}/${topdir}/protocol
+    ${RMDIR} ${stagedir}${prefix}/${_docdir}/${topdir}
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
