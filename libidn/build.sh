5230386d4dd3803db89ecff0b4bec175187cf58e
Tom G. Christensen <tgc@jupiterrise.com>
2005-02-12 18:25:04 +0000

    libidn for Irix 6.2

---
+++ libidn/build.sh
@@ -0,0 +1,60 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libidn
+version=0.5.13
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/local/include"
+export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
+set_configure_args '--prefix=$prefix --with-libiconv-prefix=/usr/local --with-libintl-prefix=/usr/local --enable-nls --enable-rpath'
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc README README-alpha TODO NEWS COPYING COPYING.LIB FAQ THANKS AUTHORS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
