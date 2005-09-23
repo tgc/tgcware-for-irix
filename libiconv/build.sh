9cf25f70e87c607d6261dfe3aed20cbed1c3eb1f
Tom G. Christensen <tgc@jupiterrise.com>
2005-09-23 18:08:06 +0000

    Switch to /usr/tgcware Update to new-style build.sh

--- libiconv/build.sh
+++ libiconv/build.sh
@@ -3,13 +3,14 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=libiconv
 version=1.9.2
-pkgver=5
+pkgver=8
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -17,11 +18,9 @@ source[0]=$topdir-$version.tar.gz
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export LDFLAGS="-L$prefix/lib -Wl,-rpath,$prefix/lib"
+set_configure_args '--prefix=$prefix --mandir=$prefix/${_mandir} --enable-extra-encodings'
 
 reg prep
 prep()
@@ -32,8 +31,6 @@ prep()
 reg build
 build()
 {
-    export LDFLAGS="-L$prefix/lib -Wl,-rpath,$prefix/lib"
-    set_configure_args '--prefix=$prefix --mandir=$prefix/${_mandir} --enable-extra-encodings'
     generic_build
 }
 
@@ -42,7 +39,8 @@ install()
 {
     generic_install DESTDIR
     doc NOTES ChangeLog DESIGN NEWS
-    ${RM} -f ${stagedir}${prefix}/${_libdir}/charset.alias
+    # Nuke "html" manpages :(
+    ${RM} -rf ${stagedir}${prefix}/${_docdir}/$topdir
 }
 
 reg pack
@@ -60,42 +58,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
