da10ff00caefee43a2ec41f5bd235c5c4887e8f1
Tom G. Christensen <tgc@jupiterrise.com>
2005-09-23 20:29:17 +0000

    Update to 1.15.1 Add trio support for vsnprintf Update to new-style build.sh Autoupdate environment information in relnotes

--- tar/build.sh
+++ tar/build.sh
@@ -3,25 +3,24 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=tar
-version=1.14
-pkgver=3
+version=1.15.1
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=tar-1.15.1-trio.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+export CPPFLAGS="-I/usr/tgcware/include"
 
 reg prep
 prep()
@@ -32,6 +31,9 @@ prep()
 reg build
 build()
 {
+    setdir ${srcdir}/${topsrcdir}
+    aclocal -I ./m4
+    autoconf
     generic_build
 }
 
@@ -59,42 +61,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
