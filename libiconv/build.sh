03159b6fc7f06e92215b141f1d2d3c9dd431f51d
Tom G. Christensen <tgc@jupiterrise.com>
2005-09-24 18:20:04 +0000

    Switch to auto update of env and deps in relnotes Add missing gettext dependency

--- libiconv/build.sh
+++ libiconv/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=libiconv
 version=1.9.2
-pkgver=8
+pkgver=9
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
