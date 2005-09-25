8d0a53c8a1d090c4c1d488f2ef2013bcc72f97d2
Tom G. Christensen <tgc@jupiterrise.com>
2005-09-25 14:14:33 +0000

    Update to 6.4 Update to new-style build.sh Update relnotes to deps & env autoupdate style

--- pcre/build.sh
+++ pcre/build.sh
@@ -3,12 +3,13 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=pcre
-version=5.0
+version=6.4
 pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -18,14 +19,7 @@ source[0]=$topdir-$version.tar.bz2
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export LDFLAGS="-Wl,-rpath,$prefix/lib -L.libs"
-export CFLAGS="-g -O2 -Wl,-rpath,$prefix/lib -L.libs"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+export LDFLAGS="-Wl,-rpath,$prefix/lib"
 
 reg prep
 prep()
@@ -36,13 +30,7 @@ prep()
 reg build
 build()
 {
-    setdir source
-    ${MKDIR} .libs
     generic_build
-    # Workaround libtool problems...
-    setdir source
-    ${SED} -e "s# libpcre.la# -L$srcdir/$topsrcdir/.libs libpcre.la#g" libpcreposix.la > libpcreposix.la.new
-    ${MV} libpcreposix.la.new libpcreposix.la
 }
 
 reg install
@@ -67,42 +55,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
