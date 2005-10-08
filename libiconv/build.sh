2e1709e9f7174229168b7e15e6e29742b857d89f
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-08 15:26:26 +0000

    Rebuild with autodeps and new versioning

--- libiconv/build.sh
+++ libiconv/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=libiconv
 version=1.9.2
-pkgver=9
+pkgver=10
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
