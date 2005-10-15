f99cbc0ab6ccd7fa0170b17f924e68c8b7d70412
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-15 10:53:35 +0000

    Rebuild trio with libtool support Redid the needtrio.m4 patch Added automake patch

--- trio/build.sh
+++ trio/build.sh
@@ -10,13 +10,11 @@
 # Check the following 4 variables before running the script
 topdir=trio
 version=1.10
-pkgver=5
+pkgver=6
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=trio-1.10-destdir.patch
-patch[1]=trio-1.10-cvs-makefile.patch
-patch[2]=trio-1.10-shlib.patch
-patch[3]=trio-1.10-needtrio.m4.patch
+patch[0]=trio-1.10-automake.patch
+patch[1]=trio-1.10-needtrio.m4.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -25,6 +23,11 @@ reg prep
 prep()
 {
     generic_prep
+    setdir source
+    libtoolize --copy
+    aclocal
+    automake --foreign --add-missing --copy
+    autoconf
 }
 
 reg build
