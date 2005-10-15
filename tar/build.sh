4ba0bfc2b01983b87bbd079d33b1c4d786dee092
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-15 11:44:49 +0000

    Rebuild with new versioning and autodeps Updated to trio patch to use the system needtrio.m4

--- tar/build.sh
+++ tar/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=tar
 version=1.15.1
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=tar-1.15.1-trio.patch
@@ -32,7 +32,8 @@ reg build
 build()
 {
     setdir ${srcdir}/${topsrcdir}
-    aclocal -I ./m4
+    aclocal -I ./m4 -I /usr/tgcware/share/aclocal
+    automake --gnu
     autoconf
     generic_build
 }
