a2537efd4fc214647f21acf8ba5993b0ab8aad56
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-22 13:40:25 +0000

    Update to 1.2.3

--- zlib/build.sh
+++ zlib/build.sh
@@ -3,12 +3,13 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=zlib
-version=1.2.2
+version=1.2.3
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -17,14 +18,11 @@ source[0]=$topdir-$version.tar.bz2
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# New default configure args
-set_configure_args '--shared --prefix=$prefix'
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+configure_args='--shared --prefix=$prefix' 
+export LDSHARED="gcc -shared -rpath ${prefix}/${_libdir} -Wl,-soname,libz.so.1"
+check_ac=0
+shortroot=1
 
 reg prep
 prep()
@@ -35,7 +33,6 @@ prep()
 reg build
 build()
 {
-    export LDSHARED="gcc -shared -rpath ${prefix}/${_libdir} -Wl,-soname,libz.so.1"
     generic_build
     setdir source
     $MAKE_PROG test
@@ -48,8 +45,7 @@ install()
     setdir source
     doc README ChangeLog algorithm.txt minigzip.c example.c
     setdir stage
-    $MV ${_includedir} ${_libdir} share/${_mandir} ${stagedir}${prefix}
-    $RMDIR share
+    $MV ${stagedir}${prefix}/share/${_mandir} ${stagedir}${prefix}
 }
 
 reg pack
@@ -67,42 +63,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
