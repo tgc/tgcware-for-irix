25500526066721666a2cad17a7774623ef60b0cb
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-22 13:51:13 +0000

    Rebuild with autodeps

--- libjpeg/build.sh
+++ libjpeg/build.sh
@@ -3,13 +3,14 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=libjpeg
 version=6b
-pkgver=5
+pkgver=7
 source[0]=jpegsrc.v6b.tar.gz
 # If there are no patches, simply comment this
 patch[0]=jpeg-c++.patch
@@ -19,15 +20,15 @@ patch[2]=libjpeg-6b-soname.patch
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
+# Global settings
+check_ac=0
 shortroot=1
+mipspro=1
 topsrcdir=jpeg-$version
-set_configure_args '--prefix=$prefix --disable-static --enable-shared'
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+configure_args='--prefix=$prefix --disable-static --enable-shared'
+export LDFLAGS='-rpath /usr/tgcware/lib'
+export INSTALL="/usr/tgcware/bin/install -c -D"
+export CC=cc
 
 reg prep
 prep()
@@ -38,10 +39,8 @@ prep()
 reg build
 build()
 {
-    export LDFLAGS='-rpath /usr/local/lib'
-    export INSTALL="/usr/local/bin/install -c -D"
     setdir source
-    ./configure $(_upls $configure_args)
+    $__configure $(_upls $configure_args)
     LD_LIBRARY_PATH=$PWD $MAKE_PROG test
     $MAKE_PROG
 }
@@ -68,42 +67,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
