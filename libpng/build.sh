495a3055a60111c5e57d256dea93bb10763c0fcb
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-22 17:15:04 +0000

    Rebuilt with autodeps

--- libpng/build.sh
+++ libpng/build.sh
@@ -3,29 +3,20 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=libpng
 version=1.2.8
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=libpng-1.2.5-makefile.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-# Build system is *horrible* for libpng
-# The makefile for IRIX/gcc was broken
-# and has been heavily patched to work :(
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
 
 reg prep
 prep()
@@ -37,7 +28,8 @@ reg build
 build()
 {
     setdir source
-    cp scripts/makefile.sggcc Makefile
+    ${CP} scripts/makefile.sggcc Makefile
+    $GSED -i "/^prefix/s;=.*;=${prefix};" Makefile
     $MAKE_PROG 
 }
 
@@ -45,7 +37,7 @@ reg install
 install()
 {
     clean stage
-    mkdir -p $stagedir$prefix
+    mkdir -p ${stagedir}${prefix}
     setdir source
     $MAKE_PROG DESTDIR=$stagedir install
     custom_install=1
@@ -68,42 +60,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
