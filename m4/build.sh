6f7932fa0994fb22d106a863253e6fd8a1f75790
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-23 11:39:37 +0000

    Update to 1.4.4

--- m4/build.sh
+++ m4/build.sh
@@ -3,27 +3,24 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=m4
-version=1.4.2
+version=1.4.4
 pkgver=1
-source[0]=$topdir-$version.tar.gz
+source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-#patch[0]=m4-1.4.1-sec.patch
-#patch[1]=m4-1.4.1-configure.patch
-
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export CC=cc
+mipspro=1
 
 reg prep
 prep()
@@ -40,12 +37,12 @@ build()
 reg install
 install()
 {
-    clean stage
-    setdir source
-    ${MKDIR} -p ${stagedir}${prefix}
-    $MAKE_PROG prefix=${stagedir}${prefix} INSTALL_DATA="$GINSTALL -c -m644" install
-    custom_install=1
     generic_install DESTDIR
+    #clean stage
+    #setdir source
+    #${MKDIR} -p ${stagedir}${prefix}
+    #$MAKE_PROG prefix=${stagedir}${prefix} INSTALL_DATA="$GINSTALL -c -m644" install
+    #custom_install=1
     doc ChangeLog NEWS README
 }
 
@@ -64,42 +61,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
