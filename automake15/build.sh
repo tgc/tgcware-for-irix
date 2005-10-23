5ba53142c52e8ef51fd4b5d4390a12a1025b2bda
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-23 10:01:57 +0000

    Update to modern std

--- automake15/build.sh
+++ automake15/build.sh
@@ -1,16 +1,17 @@
 #!/usr/local/bin/bash
-#
+
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
-topdir=automake15
+topdir=automake
 version=1.5
-pkgver=4
-source[0]=automake-$version.tar.bz2
+pkgver=5
+source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=automake-1.5-depout-mf.patch
 patch[1]=automake15-versioning.patch
@@ -20,44 +21,31 @@ patch[3]=automake-1.5-subdirs-89619.patch
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="GNU Automake"
-
-topsrcdir=automake-$version
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export PERL=/usr/tgcware/bin/perl
+configure_args="--prefix=$prefix --program-suffix=-$version"
 
 reg prep
 prep()
 {
     generic_prep
+    setdir source
+    autoreconf
 }
 
 reg build
 build()
 {
-    setdir source
-    autoconf-2.57
-    ./configure --prefix=/usr/local --program-suffix=-$version --disable-nls
-    $MAKE_PROG
+    generic_build
 }
 
 reg install
 install()
 {
     generic_install DESTDIR
-    $RM -rf $stagedir$prefix/info
-    $RM -f $stagedir$prefix/bin/aclocal $stagedir$prefix/bin/automake
-    setdir source
-    $MKDIR -p info
-    $CP automake.info* info
-    $GZIP -9nf info/*
-    doc AUTHORS COPYING ChangeLog INSTALL NEWS README THANKS TODO info
+    ${RM} -rf ${stagedir}${prefix}/${_infodir}
+    ${RM} -f ${stagedir}${prefix}/${_bindir}/{automake,aclocal}
+    doc AUTHORS COPYING ChangeLog INSTALL NEWS README THANKS TODO
 }
 
 reg pack
@@ -75,42 +63,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
