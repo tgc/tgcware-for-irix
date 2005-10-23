db43149003895f7e854a57730808633500001f05
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-23 11:08:17 +0000

    Update to current std

--- automake17/build.sh
+++ automake17/build.sh
@@ -1,27 +1,25 @@
 #!/usr/local/bin/bash
-#
+
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=automake
-version=1.7.8
-pkgver=2
-source[0]=automake-$version.tar.bz2
+version=1.7.9
+pkgver=3
+source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export PERL=/usr/tgcware/bin/perl
 
 reg prep
 prep()
@@ -32,7 +30,6 @@ prep()
 reg build
 build()
 {
-    export PERL=/usr/local/bin/perl5
     generic_build
 }
 
@@ -40,7 +37,8 @@ reg install
 install()
 {
     generic_install DESTDIR
-    ${RM} -f ${stagedir}${prefix}/${_infodir}/dir
+    ${RM} -rf ${stagedir}${prefix}/${_infodir}
+    ${RM} -f ${stagedir}${prefix}/${_bindir}/{automake,aclocal}
     doc AUTHORS COPYING ChangeLog INSTALL NEWS README THANKS TODO
 }
 
@@ -59,42 +57,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
