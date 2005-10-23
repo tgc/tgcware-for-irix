41f1861025f36f5587bb5845ba2e1a17bd2a8b86
Tom G. Christensen <tgc@jupiterrise.com>
2005-10-23 12:50:57 +0000

    Update to 2.6.6

--- rsync/build.sh
+++ rsync/build.sh
@@ -3,12 +3,13 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=rsync
-version=2.6.3
+version=2.6.6
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -17,14 +18,10 @@ source[0]=$topdir-$version.tar.gz
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Setup configure args globally so relnotes.txt is always updated from relnotes
-set_configure_args '--prefix=${prefix} --with-rsyncd-conf=${prefix}/${_sysconfdir}/rsyncd.conf --disable-ipv6'
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+export CPPFLAGS="-I/usr/tgcware/include"
+configure_args='--prefix=${prefix} --with-rsyncd-conf=${prefix}/${_sysconfdir}/rsyncd.conf --disable-ipv6'
 
 reg prep
 prep()
@@ -35,8 +32,6 @@ prep()
 reg build
 build()
 {
-    export LDFLAGS="-Wl,-rpath,/usr/local/lib -L/usr/local/lib"
-    export CPPFLAGS="-I/usr/local/include"
     generic_build
 }
 
@@ -44,7 +39,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc NEWS README TODO
+    doc NEWS README TODO COPYING
 }
 
 reg pack
@@ -62,42 +57,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
