14b2b0f82c609787c0403d5a317df4d4ccffdf5f
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-05 19:57:22 +0000

    Update to 1.1.1 Rebuild

--- libvorbis/build.sh
+++ libvorbis/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=libvorbis
-version=1.1.0
+version=1.1.1
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -19,8 +19,8 @@ source[0]=$topdir-$version.tar.gz
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 
 reg prep
 prep()
