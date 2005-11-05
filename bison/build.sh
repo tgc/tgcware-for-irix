2fbbc6f54feda8c1134be2c2b64b6bafcb1ca1cc
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-05 15:24:54 +0000

    Update to 2.1 Update to newstyle pkg

--- bison/build.sh
+++ bison/build.sh
@@ -3,13 +3,14 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=bison
-version=1.875
-pkgver=2
+version=2.1
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -17,11 +18,7 @@ source[0]=$topdir-$version.tar.bz2
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
 
 reg prep
 prep()
@@ -40,7 +37,7 @@ install()
 {
     generic_install DESTDIR
     $RM -f $stagedir$prefix/info/dir
-    doc ChangeLog TODO
+    doc ChangeLog TODO COPYING
 }
 
 reg pack
@@ -58,42 +55,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
