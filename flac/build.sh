245f32aa08c1e0a2cb4bfc3040e64c79b44d07d5
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-06 12:21:34 +0000

    Redo snprintf patch to use trio Rebuild

--- flac/build.sh
+++ flac/build.sh
@@ -13,21 +13,27 @@ version=1.1.2
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=flac-1.1.2-snprintf.patch
+#patch[0]=flac-1.1.2-snprintf.patch
+patch[0]=flac-1.1.2-needstrio.patch
+patch[1]=flac-1.1.2-noxmms.patch
+patch[2]=flac-1.1.2-include.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-
-set_configure_args '--prefix=$prefix --disable-rpath'
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args='--prefix=$prefix --disable-rpath'
 
 reg prep
 prep()
 {
     generic_prep
+    aclocal-1.9
+    automake-1.9
+    autoheader
+    autoconf
     # The libiconv stuff is broken :(
     # This needs GNU sed with inplace editing!
     $FIND . -name 'Makefile.in' -print|$XARGS -n 1 $GSED -i 's/@LIBICONV@/@LTLIBICONV@/g'
