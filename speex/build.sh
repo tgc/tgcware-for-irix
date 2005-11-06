9b26e8f912b7a6382598426974db0e82faac1e58
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-06 12:21:56 +0000

    Update to 1.0.5 Rebuild

--- speex/build.sh
+++ speex/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=speex
-version=1.0.4
+version=1.0.5
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -19,17 +19,17 @@ patch[0]=speex-1.0.4-getopt.patch
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-
-set_configure_args '--prefix=$prefix --mandir=${prefix}/${_mandir}'
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args='--prefix=$prefix --mandir=${prefix}/${_mandir} --with-ogg-dir=$prefix'
 
 reg prep
 prep()
 {
     generic_prep
-    aclocal # 1.7.x is ok
-    automake # ditto
+    setdir source
+    aclocal-1.9 # 1.7.x is ok
+    automake-1.9 # ditto
     autoconf # 2.49+
 }
 
@@ -43,7 +43,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc AUTHORS README TODO
+    doc AUTHORS README TODO COPYING
     ${MV} ${stagedir}${prefix}/${_sharedir}/man ${stagedir}${prefix}
 }
 
