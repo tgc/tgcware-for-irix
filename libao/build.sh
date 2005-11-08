16500048d0f05a8e3d278d11baeb4e6dda3e9bb2
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-08 20:05:33 +0000

    Update old-style audio API patches Rebuild

--- libao/build.sh
+++ libao/build.sh
@@ -10,26 +10,28 @@
 # Check the following 4 variables before running the script
 topdir=libao
 version=0.8.6
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=libao-0.8.6-irix62-plugin.patch
+patch[0]=libao-0.8.6-oldirix-support.patch
+patch[1]=libao-0.8.6-irixcheck.patch
+patch[2]=libao-0.8.6-needstrio.patch
+patch[3]=libao-0.8.6-trio-include.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-
-set_configure_args '--prefix=$prefix --disable-esd --disable-polyp'
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args='--prefix=$prefix --disable-esd --disable-polyp'
 
 reg prep
 prep()
 {
     generic_prep
-    aclocal
-    automake -a -c
+    aclocal-1.9
+    automake-1.9 --foreign -a -c
     autoconf
 }
 
@@ -43,7 +45,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc TODO README CHANGES
+    doc TODO README CHANGES COPYING AUTHORS
     ${RM} -f ${stagedir}${prefix}/${_libdir}/ao/plugins-2/lib*.la
 }
 
