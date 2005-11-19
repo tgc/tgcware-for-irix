27d06468f7a864229a866e9708c9b0a08134159a
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-19 15:06:48 +0000

    Rebuild

--- fltk/build.sh
+++ fltk/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=fltk
 version=1.1.6
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version-source.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=fltk-1.1.6-fontconfig.patch
@@ -19,9 +19,9 @@ patch[0]=fltk-1.1.6-fontconfig.patch
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-set_configure_args '--prefix=$prefix --enable-shared --enable-thread --enable-xft'
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args='--prefix=$prefix --enable-shared --enable-thread --enable-xft'
 
 reg prep
 prep()
