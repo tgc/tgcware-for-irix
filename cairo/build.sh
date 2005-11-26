1fd4e79a721d4f8566bf8510e8aad8e5807d0126
Tom G. Christensen <tgc@jupiterrise.com>
2005-11-26 19:16:14 +0000

    Cairo 1.0.2 for Irix 6.2

---
+++ cairo/build.sh
@@ -0,0 +1,64 @@
+#!/usr/local/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=cairo
+version=1.0.2
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+patch[0]=cairo-1.0.2-nostdint.patch
+patch[1]=cairo-1.0.2-trio.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    aclocal-1.9
+    autoheader
+    autoconf
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc README NEWS COPYING* TODO
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
