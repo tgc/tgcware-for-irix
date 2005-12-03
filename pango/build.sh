eb01b4ce231d826a4f4a603c883c80cf826da819
Tom G. Christensen <tgc@jupiterrise.com>
2005-12-03 18:25:26 +0000

    Update to 1.10.2

--- pango/build.sh
+++ pango/build.sh
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=pango
-version=1.8.0
-pkgver=2
+version=1.10.2
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -19,8 +19,8 @@ source[0]=$topdir-$version.tar.bz2
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include"
-export LDFLAGS="-L/usr/local/lib"
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 
 META_CLEAN="$META_CLEAN ops"
 
@@ -39,21 +39,17 @@ build()
 reg install
 install()
 {
-    # We need to fix up some libtool garbage before installing :(
-    setdir source
-    cd pango
-    perl -pi -e 's|Wl,/usr/people/tgc/build/athena-buildpkg/buildpkg/pango/src/pango-1.8.0/pango/.libs:|Wl,|g' pango-querymodules
     # Done, now install
     generic_install DESTDIR
-    doc NEWS README
+    doc NEWS README COPYING AUTHORS
     # Add empty pango.modules so that the package will own it (it's filled out by ops)
     touch ${stagedir}${prefix}/${_sysconfdir}/pango/pango.modules
+    echo "lastop exitop(${prefix}/${_bindir}/pango-querymodules > ${prefix}/${_sysconfdir}/pango/pango.modules)" > $metadir/ops
 }
 
 reg pack
 pack()
 {
-    echo "lastop exitop(${prefix}/${_bindir}/pango-querymodules > ${prefix}/${_sysconfdir}/pango/pango.modules)" > $metadir/ops
     generic_pack
 }
 
