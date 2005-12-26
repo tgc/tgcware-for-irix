23ed122b0a48b30c8e1db6be52df358e1a95fa70
Tom G. Christensen <tgc@jupiterrise.com>
2005-12-26 09:47:09 +0000

    Update to 3.1 Build with gcc

--- bash3/build.sh
+++ bash3/build.sh
@@ -9,26 +9,11 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=bash
-version=3.0
+version=3.1
 pkgver=1
 source[0]=bash-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=bash30-001
-patch[1]=bash30-002
-patch[2]=bash30-003
-patch[3]=bash30-004
-patch[4]=bash30-005
-patch[5]=bash30-006
-patch[6]=bash30-007
-patch[7]=bash30-008
-patch[8]=bash30-009
-patch[9]=bash30-010
-patch[10]=bash30-011
-patch[11]=bash30-012
-patch[12]=bash30-013
-patch[13]=bash30-014
-patch[14]=bash30-015
-patch[15]=bash30-016
+patch[0]=bash31-001
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -37,9 +22,9 @@ patch[15]=bash30-016
 patchdir=$srcfiles
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-export CC=cc
+#export CC=cc
 configure_args="--prefix=$prefix --disable-rpath"
-mipspro=1
+#mipspro=1
 
 reg prep
 prep()
