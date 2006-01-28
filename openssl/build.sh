8c0c578ccbac3e8320512aee3415c052b13eca29
Tom G. Christensen <tgc@jupiterrise.com>
2006-01-28 17:44:46 +0000

    Update to 0.9.8

--- openssl/build.sh
+++ openssl/build.sh
@@ -3,36 +3,30 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=openssl
-version=0.9.7e
+version=0.9.8a
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=openssl-0.9.7e-fips_rand.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Version specific patch
-[ "$_os" == "irix53" ] && patch[2]=openssl-0.9.7d-irix53-ld.patch
-
-# Custom subsystems
-subsysconf=$metadir/subsys.conf
-
-sover=5 # e = 5
-abbrev_ver=$(echo $version|$SED -e 's/\.//g' -e 's/[a-zA-Z]//g')
-baseversion=$(echo $version|$SED -e 's/[a-zA-Z]//g')
-specver="$(fix_ver "${version}${sover}-${pkgver}")"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+ignore_deps="tgc_perl5.sw.base"
+check_ac=0
+__configure="./Configure"
+mipspro=1
+shared_args="--prefix=$prefix --openssldir=$prefix/ssl zlib shared"
+[ "$_os" == "irix53" ] && configure_args="irix-mips1-gcc $shared_args"
+[ "$_os" == "irix62" ] && configure_args="irix-mips3-cc $shared_args"
+#set_configure_args="$configure_args"
 
 reg prep
 prep()
@@ -43,22 +37,12 @@ prep()
 reg build
 build()
 {
-    global_config_args="--prefix=$prefix --openssldir=$prefix/ssl no-fips shared"
-    if [ "$_os" = "irix53" ]; then
-	configure_args="irix-mips1-gcc $global_config_args"
-    fi
-    if [ "$_os" = "irix62" ]; then
-	configure_args="irix-mips3-gcc $global_config_args"
-    fi
-    set_configure_args "$configure_args"
-
     setdir source
-    $SED -e "s;@LIBDIR@;${prefix}/lib;g" Makefile.org > Makefile.new
-    $MV -f Makefile.new Makefile.org
-
-    ./Configure $configure_args
-
-    $MAKE_PROG LIBSSL="-Wl,-rpath,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-rpath,$prefix/lib -L.. -lcrypto" all
+    $__configure $configure_args
+    $GSED -i '/EX_LIBS/s;-lz;-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib -lz;' Makefile
+    $GSED -i '/^CFLAG=/s;.*=;CFLAG= -I/usr/tgcware/include;' Makefile
+    $MAKE_PROG SHARED_LDFLAGS="-Wl,-rpath,${prefix}/${_libdir}" depend
+    $MAKE_PROG SHARED_LDFLAGS="-Wl,-rpath,${prefix}/${_libdir}"
 }
 
 reg install
@@ -66,110 +50,41 @@ install()
 {
     clean stage
     setdir source
-    $MAKE_PROG INSTALL_PREFIX=$stagedir LIBSSL="-Wl,-rpath,$prefix/lib -L.. -lssl" LIBCRYPTO="-Wl,-rpath,$prefix/lib -L.. -lcrypto" install
-    setdir ${stagedir}${prefix}/${_libdir}
-    chmod a+x pkgconfig
-    ${RM} -f libfips* lib*.so.0
-    ${RM} -f lib*.so
-    ${LN} -s libssl.so.$baseversion libssl.so
-    ${LN} -s libcrypto.so.$baseversion libcrypto.so
-    $MV ${stagedir}${prefix}/ssl/${_mandir} ${stagedir}${prefix}
+    $MAKE_PROG INSTALL_PREFIX=$stagedir MANDIR=${prefix}/${_mandir} install
     setdir ${stagedir}${prefix}/${_mandir}
     for j in $($LS -1d man?)
     do
-	cd $j
-	for manpage in *
-	do
-	    if [ -L "${manpage}" ]; then
-		TARGET=`$LS -l "${manpage}" | $AWK '{ print $NF }'`
-		$LN -sf "${TARGET}"ssl "${manpage}"ssl
-		$RM -f "${manpage}"
-	    else
-		$MV "$manpage" "$manpage""ssl"
-	    fi
-	done
-	cd ..
+        cd $j
+        for manpage in *
+        do
+            if [ -L "${manpage}" ]; then
+                TARGET=`$LS -l "${manpage}" | $AWK '{ print $NF }'`
+                $LN -sf "${TARGET}"ssl "${manpage}"ssl
+                $RM -f "${manpage}"
+            else
+                $MV "$manpage" "$manpage""ssl"
+            fi
+        done
+        cd ..
     done
     doc README CHANGES FAQ INSTALL LICENSE NEWS
     custom_install=1
-    generic_install
+    generic_install INSTALL_PREFIX
 }
 
 reg pack
 pack()
 {
-    # Arrgh! This is a mess :(
-    clean meta
-    setdir ${stagedir}${prefix}/man      
-    fix_man
-    compress_man
-    setdir ${stagedir}${prefix}
-    metaprefix=${metainstalldir##$topinstalldir}
-    metaprefix="${metaprefix#/*}"
-    [ ! -z "$metaprefix" ] && metaprefix="${metaprefix}/"
-    [ ! "$metainstalldir" == "/" ] && metainstalldir="${metainstalldir}/"
-    auto_src
-    auto_rel
-    create_idb
-    # Create a depends file
-    echo "sw.dev ${pkgname}.sw.base $specver $specver" > $metadir/depends
-    echo "sw.base ${pkgname}.sw.lib $specver $specver" >> $metadir/depends
-    create_spec
-    # Fix pkgversion
-    fixed=$(fix_ver $version-$pkgver)
-    $SED -e "s;version ${fixed};version ${specver};g" $specfile > /tmp/spec
-    $MV -f /tmp/spec $specfile
-    auto_dist
-    check_unpackaged
-    make_dist
+    generic_pack
 }
 
 reg distclean
 distclean()
 {
-    META_CLEAN="$META_CLEAN depends"
     clean distclean
 }
 
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
