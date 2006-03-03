64f40ba5a910960a345b0faa3ddd4f9e57aa53d0
Tom G. Christensen <tgc@jupiterrise.com>
2006-03-03 21:12:47 +0000

    Update to 4.1.4 Update to newstyle metadata

--- gmp/build.sh
+++ gmp/build.sh
@@ -3,30 +3,25 @@
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=gmp
-version=4.1.2
-pkgver=2
+version=4.1.4
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-patch[0]=gmp-4.1.2-cxxtests.patch
-patch[1]=gmp-fixes.patch
+#patch[0]=
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
-# Fill in pkginfo values if necessary
-# using pkgname,name,pkgcat,pkgvendor & pkgdesc
-name="GNU gmp"
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="--prefix=$prefix --enable-cxx --enable-mpbsd"
 
 reg prep
 prep()
@@ -37,18 +32,17 @@ prep()
 reg build
 build()
 {
-    export LDFLAGS="-Wl,-rpath,$prefix/lib -L.libs"
+    generic_build	
     setdir source
-    ./configure --prefix=$prefix --enable-cxx --enable-mpbsd --enable-mpfr
-    $MKDIR .libs
-    $MAKE_PROG
+    $MAKE_PROG check
 }
 
 reg install
 install()
 {
     generic_install DESTDIR
-    $RM -f $stagedir$prefix/info/dir
+    ${RM} -f ${stagedir}${prefix}/${_infodir}/dir
+    doc AUTHORS COPYING COPYING.LIB NEWS README
 }
 
 reg pack
@@ -66,42 +60,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
