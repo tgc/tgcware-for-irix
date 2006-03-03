2f79fa17bc531f4fa2c176c4d5749b869c7c8ded
Tom G. Christensen <tgc@jupiterrise.com>
2006-03-03 10:10:55 +0000

    Update to 1.0.3 Update to newstyle metadata/build.sh Change sover and add -set_version

--- bzip2/build.sh
+++ bzip2/build.sh
@@ -1,27 +1,22 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
 # 
-# The concept of "method" registering and the logic that implements it was shamelessly
-# stolen from jhlj's Compile.sh script :)
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
+###########################################################
 # Check the following 4 variables before running the script
 topdir=bzip2
-version=1.0.2
-pkgver=8
+version=1.0.3
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=bzip2-1.0.3-soname.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
-
-# Define script functions and register them
-METHODS=""
-reg() {
-    METHODS="$METHODS $1"
-}
 
 reg prep
 prep()
@@ -33,8 +28,8 @@ reg build
 build()
 {
     setdir source
-    $MAKE_PROG -f Makefile LDFLAGS="-rpath /usr/local/lib"
-    $MAKE_PROG -f Makefile-libbz2_so CC="gcc -rpath /usr/local/lib"
+    $MAKE_PROG -f Makefile LDFLAGS="-Wl,-rpath,/usr/tgcware/lib"
+    $MAKE_PROG -f Makefile-libbz2_so CC="gcc -Wl,-rpath,/usr/tgcware/lib"
 }
 
 reg install
@@ -44,7 +39,7 @@ install()
     setdir source
     ${MKDIR} -p ${stagedir}${prefix}/{${_bindir},${_mandir}/man1,${_libdir},${_includedir}}
     ${GINSTALL} -m 755 bzlib.h ${stagedir}${prefix}/${_includedir}
-    ${GINSTALL} -m 755 libbz2.so.1.0.2 ${stagedir}${prefix}/${_libdir}
+    ${GINSTALL} -m 755 libbz2.so.1.0.3 ${stagedir}${prefix}/${_libdir}
     ${GINSTALL} -m 755 libbz2.a ${stagedir}${prefix}/${_libdir}
     ${GINSTALL} -m 755 bzip2-shared  ${stagedir}${prefix}/${_bindir}/bzip2
     ${GINSTALL} -m 755 bzip2recover bzgrep bzdiff bzmore ${stagedir}${prefix}/${_bindir}/
@@ -53,8 +48,9 @@ install()
     ${LN} -s bzip2 ${stagedir}${prefix}/${_bindir}/bzcat
     ${LN} -s bzdiff ${stagedir}${prefix}/${_bindir}/bzcmp
     ${LN} -s bzmore ${stagedir}${prefix}/${_bindir}/bzless
-    ${LN} -s libbz2.so.1.0.2 ${stagedir}${prefix}/${_libdir}/libbz2.so.1.0
-    ${LN} -s libbz2.so.1.0 ${stagedir}${prefix}/${_libdir}/libbz2.so
+    ${LN} -s libbz2.so.1.0.3 ${stagedir}${prefix}/${_libdir}/libbz2.so.1
+    ${LN} -s libbz2.so.1.0.3 ${stagedir}${prefix}/${_libdir}/libbz2.so.1.0
+    ${LN} -s libbz2.so.1 ${stagedir}${prefix}/${_libdir}/libbz2.so
     ${LN} -s bzip2.1 ${stagedir}${prefix}/${_mandir}/man1/bzip2recover.1
     ${LN} -s bzip2.1 ${stagedir}${prefix}/${_mandir}/man1/bunzip2.1
     ${LN} -s bzip2.1 ${stagedir}${prefix}/${_mandir}/man1/bzcat.1
@@ -81,42 +77,4 @@ distclean()
 ###################################################
 # No need to look below here
 ###################################################
-
-reg all
-all()
-{
-    for METHOD in $METHODS 
-    do
-	case $METHOD in
-	     all*|*clean) ;;
-	     *) $METHOD
-		;;
-	esac
-    done
-
-}
-
-reg
-usage() {
-    echo Usage $0 "{"$(echo $METHODS | tr " " "|")"}"
-    exit 1
-}
-
-OK=0
-for METHOD in $*
-do
-    METHOD=" $METHOD *"
-    if [ "${METHODS%$METHOD}" == "$METHODS" ] ; then
-	usage
-    fi
-    OK=1
-done
-
-if [ $OK = 0 ] ; then
-    usage;
-fi
-
-for METHOD in $*
-do
-    ( $METHOD )
-done
+build_sh $*
