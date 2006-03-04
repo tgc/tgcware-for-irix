7072b56de780bb5a5af72edf746f65aa7070084d
Tom G. Christensen <tgc@jupiterrise.com>
2006-03-04 18:24:53 +0000

    Update to 1.10.4

--- pango/build.sh
+++ pango/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=pango
-version=1.10.2
+version=1.10.4
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
