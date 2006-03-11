1753843566806183c41eaab9ed9720ac7ade0181
Tom G. Christensen <tgc@jupiterrise.com>
2006-03-11 08:57:09 +0000

    Update to 2.8.6

--- glib2/build.sh
+++ glib2/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=glib
-version=2.8.4
-pkgver=2
+version=2.8.6
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
