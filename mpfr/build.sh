55f6462dade18d684fd63fa5b930ed1abaf2e5d0
Tom G. Christensen <tgc@jupiterrise.com>
2006-03-16 18:13:13 +0000

    mpfr 2.2.0 for Irix

---
+++ mpfr/build.sh
@@ -0,0 +1,62 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=mpfr
+version=2.2.0
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="--prefix=$prefix --with-gmp=$prefix --enable-shared --disable-dependency-tracking"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+    setdir source
+    $MAKE_PROG check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS BUGS COPYING* FAQ.html NEWS TODO
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
