5a1b56e3a08f62095068bce34f7528c3f7390bc0
Tom G. Christensen <tgc@jupiterrise.com>
2006-04-05 18:53:15 +0000

    lzo 2.02 for Irix 6.2

---
+++ lzo/build.sh
@@ -0,0 +1,69 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=lzo
+version=2.02
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --enable-shared"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+    setdir source
+    # ugly ugly hack but otherwise it complains about copysignl being
+    # undefined in libgcc_s.so.1
+    $GSED -i '/^dependency/s;'' ;'' -lm ;' src/liblzo2.la
+    $MAKE_PROG
+    $MAKE_PROG check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    $GINSTALL -D -m 755 $srcdir/$topsrcdir/examples/.libs/lzopack ${stagedir}${prefix}/${_bindir}/lzopack
+    doc AUTHORS BUGS COPYING NEWS THANKS doc
+    custom_install=1
+    generic_install
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
