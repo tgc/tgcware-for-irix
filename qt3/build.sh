72e2479abe867b1cd981df9c7b2cf2f31d49cba4
Tom G. Christensen <tgc@jupiterrise.com>
2006-04-10 14:29:44 +0000

    Update to 3.3.6 Add a few patches from the CentOS4 srpm

--- qt3/build.sh
+++ qt3/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,11 +9,13 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=qt-x11-free
-version=3.3.4
-pkgver=1
+version=3.3.6
+pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=qt-3.3.4-irix6.2-glfix.patch
+patch[1]=qt-3.1.0-makefile.patch
+patch[2]=qt-uic-nostdlib.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -22,11 +24,12 @@ patch[0]=qt-3.3.4-irix6.2-glfix.patch
 # Hardcode new prefix
 qt=qt-3.3
 prefix=$prefix/$qt
-qtdir=$prefix
-plugins_style="-qt-style-cde -qt-style-motifplus -qt-style-platinum -qt-style-sgi -qt-style-windows -qt-style-compact -qt-imgfmt-png -qt-imgfmt-jpeg -qt-imgfmt-mng"
-set_configure_args '\
+final_qtdir=$prefix
+#plugins_style="-qt-style-cde -qt-style-motifplus -qt-style-platinum -qt-style-sgi -qt-style-windows -qt-style-compact -qt-imgfmt-png -qt-imgfmt-jpeg -qt-imgfmt-mng"
+plugins_style="-qt-imgfmt-png -qt-imgfmt-jpeg -qt-imgfmt-mng"
+configure_args='\
     -platform irix-g++ \
-    -prefix $qtdir \
+    -prefix $final_qtdir \
     -release \ 
     -shared \
     -qt-gif \
@@ -37,11 +40,12 @@ set_configure_args '\
     -thread \
     -no-ipv6 \
     -no-exceptions \
-    -I/usr/local/include \
-    -L/usr/local/lib \
-    -R/usr/local/lib \
-    -L$qtdir/${_libdir} \
-    -R$qtdir/${_libdir} \
+    -no-g++-exceptions \
+    -I/usr/tgcware/include \
+    -L/usr/tgcware/lib \
+    -R/usr/tgcware/lib \
+    -L$final_qtdir/${_libdir} \
+    -R$final_qtdir/${_libdir} \
     $plugin_style'
 
 # Manpages uses .so style linking but the autoformatter doesn't handle that
@@ -51,25 +55,44 @@ reg prep
 prep()
 {
     generic_prep
+    # Add -rpath to qmake build
+    setdir source
+    $GSED -i '/^LFLAGS/s;\(.*\);\1 -Wl,-rpath,/usr/tgcware/lib;' qmake/Makefile.unix
+    # Make sure capability tests are linked with rpath (so they can find libstdc++)
+    setdir config.tests
+    for i in $($FIND . -name '*.pro')
+    do
+	echo "LIBS+=-Wl,-rpath,/usr/tgcware/lib" >> $i
+    done
 }
 
 reg build
 build()
 {
+    echo "Build started: $(date)"
     setdir source
     export QTDIR=`pwd`
     export LD_LIBRARYN32_PATH="$QTDIR/lib"
     export PATH="$QTDIR/bin:$PATH"
-    export QTDEST=$qtdir
+    export QTDEST=$final_qtdir
     
-    # don't use rpath
-    perl -pi -e "s|-Wl,-rpath,|-L|" mkspecs/*/qmake.conf
-
     # Argh! It asks a question before running configure :(
     echo $(_upls $configure_args)
-   # echo yes | ./configure -v $(_upls $configure_args)
-    $MAKE_PROG
-    echo "HUSK ./configure ER UDKOMMENTERET!"
+    echo yes | ./configure -v $(_upls $configure_args)
+    # Remove troublesome and uneeded -rpath from Makefiles before building
+    $GSED -i '/^LFLAGS/s;\(.*mt.so.3\).*;\1;' src/Makefile
+    cd tools
+    $FIND . -name 'Makefile' | $GREP -v examples | $XARGS $GSED -i '/^LFLAGS/s;\(.*buffer=1000\).*;\1;'
+    cd ..
+    # First build qmake
+    $MAKE_PROG src-qmake
+    # Build moc
+    $MAKE_PROG src-moc
+    # Build libqt
+    $MAKE_PROG sub-src
+    # Build tools
+    $MAKE_PROG sub-tools UIC="$QTDIR/bin/uic -nostdlib -L $QTDIR/plugins"
+    echo "Build ended: $(date)"
 }
 
 reg install
@@ -78,7 +101,7 @@ install()
     export QTDIR=`pwd`
     export LD_LIBRARYN32_PATH="$QTDIR/lib"
     export PATH="$QTDIR/bin:$PATH"
-    export QTDEST=$qtdir
+    export QTDEST=$final_qtdir
     
     generic_install INSTALL_ROOT
 
@@ -88,9 +111,9 @@ install()
     $MAKE_PROG -C tutorial clean
     $MAKE_PROG -C examples clean
 
-    # don't include Makefiles of qt examples/tutorials
-    $FIND examples -name "Makefile" | $XARGS $RM -f
-    $FIND tutorial -name "Makefile" | $XARGS $RM -f
+    $FIND examples -name Makefile | xargs perl -pi -e 's|\.\./\.\.|\$\(QTDIR\)|'
+    $FIND tutorial -name Makefile | xargs perl -pi -e 's|\.\./\.\.|\$\(QTDIR\)|'
+    $FIND examples -name '*.pro' | xargs $GSED -i '/^DEPENDPATH/s|\.\./\.\./include|$\(QTDIR\)/include|'
 
     doc FAQ LICENSE.QPL README* changes*
     doc examples tutorial
