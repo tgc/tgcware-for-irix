fc3a9c8ba958ac88f71b40cff51dd2fb772dc7b5
Tom G. Christensen <tgc@jupiterrise.com>
2006-04-11 15:28:07 +0000

    Update to 5.5

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,18 +9,19 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=ncurses
-version=5.4
-pkgver=3
-source[0]=$topdir-$version.tar.bz2
+version=5.5
+pkgver=1
+source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=ncurses-5.5-destdir.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export LD="ld -n32 -rpath $prefix"
-set_configure_args '--prefix=$prefix --with-shared --without-debug --with-install-prefix=${stagedir} --with-manpage-symlinks --disable-rpath --enable-symlinks --with-manpage-format=normal --with-libtool'
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args='--prefix=$prefix --with-shared --without-debug --with-install-prefix=${stagedir} --with-manpage-symlinks --disable-rpath --enable-symlinks --with-manpage-format=normal --with-manpage-symlinks --enable-symlinks --without-ada --with-libtool'
 
 reg prep
 prep()
