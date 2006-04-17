a4abe4786468c4de071c88d09a270bcf69ccbf5a
Tom G. Christensen <tgc@jupiterrise.com>
2006-04-17 13:43:26 +0000

    Subversion 1.3.1 for Irix 6.2 Currently just client support. The dav_svn apache module is not built.

---
+++ subversion/build.sh
@@ -0,0 +1,66 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=subversion
+version=1.3.1
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+patch[0]=subversion-1.3.1-neon.patch
+patch[1]=subversion-1.3.1-norpath.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --with-neon=${prefix} --with-apr=${prefix} --with-apr-util=${prefix}"
+
+[ "$_os" = "irix62" ] && ac_overrides="ac_cv_lib_socket_socket=no"
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    autoconf
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc CHANGES COPYING INSTALL README COMMITTERS BUGS
+    $RMDIR ${stagedir}${prefix}/${_infodir}
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
