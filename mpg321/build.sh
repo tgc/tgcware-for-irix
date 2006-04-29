bc39b2619d689d7d4c34e07bc06b3603a9b921b4
Tom G. Christensen <tgc@jupiterrise.com>
2006-04-29 13:43:14 +0000

    mpg321 0.2.10 for Irix 6.2

---
+++ mpg321/build.sh
@@ -0,0 +1,66 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=mpg321
+version=0.2.10
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+patch[0]=mpg321-0.2.10-extra-checks.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --enable-mpg123-symlink=no --with-default-audio=irix"
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    $GSED -i 's/AM_PATH_AO/XIPH_PATH_AO/g' configure.ac
+    aclocal-1.9 -I m4
+    automake-1.9 -a -c
+    autoheader
+    autoconf
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS BUGS HACKING COPYING README* THANKS TODO
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
