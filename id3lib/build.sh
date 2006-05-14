e9e03c611719330898faa2fbf67ec478dfd98dac
Tom G. Christensen <tgc@jupiterrise.com>
2006-05-14 11:17:55 +0000

    id3lib 3.8.3 for Irix 6.2

---
+++ id3lib/build.sh
@@ -0,0 +1,65 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=id3lib
+version=3.8.3
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --enable-debug=no --disable-static"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS COPYING ChangeLog HISTORY NEWS README THANKS TODO
+    doc examples doc
+    setdir ${stagedir}${prefix}/${_vdocdir}
+    $RM -f doc/{Doxyfile*,Makefile*,*.in}
+    $RM -rf examples/{Makefile*,.deps/,.libs/,*.o}
+    $RM -rf examples/{id3convert,id3cp,id3info,id3tag}
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
