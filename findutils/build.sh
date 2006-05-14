f5d650a65e29dc2e03d3f4f3d2614dccc8f1e050
Tom G. Christensen <tgc@jupiterrise.com>
2006-05-14 19:04:23 +0000

    Fix build issue on Irix 5.3

--- findutils/build.sh
+++ findutils/build.sh
@@ -13,20 +13,29 @@ version=4.2.27
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=findutils-4.2.27-mbchar.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 configure_args="--prefix=$prefix --disable-rpath --with-libiconv-prefix=/usr/tgcware --with-lintl-prefix=/usr/tgcware"
 
-# Source function library
-. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+if [ "$_os" = "irix53" ]; then
+    export CC=cc
+    mipspro=2
+fi
 
 reg prep
 prep()
 {
     generic_prep
+    aclocal-1.9 -I gnulib/m4 -I m4
+    automake-1.9 --gnits
+    autoconf
+    autoheader
 }
 
 reg build
