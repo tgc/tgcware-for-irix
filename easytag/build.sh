ed862b4ae62b9a66e9cdd11d11f03a9e79282cf0
Tom G. Christensen <tgc@jupiterrise.com>
2006-05-14 12:35:58 +0000

    easytag 1.1 for Irix 6.2

---
+++ easytag/build.sh
@@ -0,0 +1,62 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=easytag
+version=1.1
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+patch[0]=easytag-1.1-snprintf.patch
+patch[1]=easytag-1.1-shutrdwr.patch
+patch[2]=easytag-1.1-bzero.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc ChangeLog INSTALL COPYING README TODO THANKS USERS-GUIDE
+    $RM -rf ${stagedir}${prefix}/${_sharedir}/$topdir
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
