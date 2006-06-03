a1a7b83f31911bfadba53e2d8a237491031e952c
Tom G. Christensen <tgc@jupiterrise.com>
2006-06-03 08:22:29 +0000

    Update to 1.1.3

--- libogg/build.sh
+++ libogg/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=libogg
-version=1.1.2
-pkgver=2
+version=1.1.3
+pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
