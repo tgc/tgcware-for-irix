45dc025d25a0569b624c8d18fb43b4e42d27ffff
Tom G. Christensen <tgc@jupiterrise.com>
2006-06-24 18:28:51 +0000

    x11vnc for Irix 6.2

---
+++ x11vnc/build.sh
@@ -0,0 +1,67 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=x11vnc
+version=0.8.1
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+patch[0]=x11vnc-0.8.1-shutrdwr.patch
+patch[1]=x11vnc-0.8.1-trio.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+ac_overrides="ac_cv_lib_socket_socket=no"
+configure_args="--prefix=$prefix --without-xkeyboard"
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    aclocal-1.9
+    automake-1.9
+    autoheader
+    autoconf
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS README
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
