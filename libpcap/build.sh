d72a48e77b7f69c4d035c8cc993bf704ecd02b0c
Tom G. Christensen <tgc@jupiterrise.com>
2006-06-24 12:29:14 +0000

    libpcap 0.9.4 for Irix 6.2

---
+++ libpcap/build.sh
@@ -0,0 +1,65 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libpcap
+version=0.9.4
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+patch[0]=libpcap-shared.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+major=0
+minor=9
+subminor=4
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    $GSED -e "/^MAJOR/s|\$(LIBMAJOR)|$major|" -e "/^MINOR/s|\$(LIBMINOR)|$minor|" -e "/^SUBMINOR/s|\$(LIBSUBMINOR)|$subminor|" Makefile.in > Makefile.in.new
+    $MV Makefile.in.new Makefile.in
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc LICENSE CHANGES CREDITS TODO
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
