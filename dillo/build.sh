01265f3ea0202169c057dcf5d6add1e9aff5d461
Tom G. Christensen <tgc@jupiterrise.com>
2006-06-28 18:42:21 +0000

    Update LDFLAGS

--- dillo/build.sh
+++ dillo/build.sh
@@ -20,7 +20,7 @@ source[0]=$topdir-$version.tar.bz2
 
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
-export LDFLAGS="-L/usr/tgcware/lib -rpath /usr/tgcware/lib"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 
 reg prep
 prep()
