2696ec719106c910ae1a7e30bfffe2f8b502cde3
Tom G. Christensen <tgc@jupiterrise.com>
2006-09-18 18:24:35 +0000

    Update to 1.2.11

--- gnutls/build.sh
+++ gnutls/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gnutls
-version=1.2.10
+version=1.2.11
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
