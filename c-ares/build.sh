9f509bb03b94bcd24054174f3337218ec8f94935
Tom G. Christensen <tgc@jupiterrise.com>
2006-09-24 11:37:27 +0000

    c-ares 1.3.1 for Irix 6.2

---
+++ c-ares/build.sh
@@ -0,0 +1,59 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=c-ares
+version=1.3.1
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+configure_args="$configure_args --mandir=${prefix}/${_mandir} --disable-static --enable-shared"
+ac_overrides="ac_cv_func_inet_pton=no ac_cv_func_inet_ntop=no"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS NEWS CHANGES README README.cares
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
