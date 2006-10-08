39d15fe4abb9ced6825b6252ad31e57da26f01c1
Tom G. Christensen <tgc@jupiterrise.com>
2006-10-08 15:32:09 +0000

    rdesktop 1.5.0 for Irix 6.2

---
+++ rdesktop/build.sh
@@ -0,0 +1,65 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=rdesktop
+version=1.5.0
+pkgver=1
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+patch[0]=rdesktop-1.5.0-trio.patch
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+[ "$_os" = "irix53" ] && patch[1]=rdesktop-1.4.1-no-CODESET.patch
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="--prefix=$prefix --with-openssl=$prefix --with-egd-socket=/var/run/egd-pool --with-sound=libao --with-libiconv-prefix=$prefix"
+
+reg prep
+prep()
+{
+    generic_prep
+    setdir source
+    aclocal-1.9
+    autoconf
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING README doc
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
