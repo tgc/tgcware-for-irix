3a4f91e25d825924cd627a04eba0a5e3b541214a
Tom G. Christensen <tgc@jupiterrise.com>
2006-10-18 19:26:32 +0000

    Switch to MIPSpro for bot 5.3 & 6.2

--- bison/build.sh
+++ bison/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=bison
 version=2.3
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -21,6 +21,9 @@ source[0]=$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+export CC=cc
+[ "$_os" = "irix53" ] && mipspro=2
+[ "$_os" = "irix62" ] && mipspro=1
 
 reg prep
 prep()
