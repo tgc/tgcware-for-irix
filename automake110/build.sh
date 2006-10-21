67a1dd37bffc9219aa4290151f938ad71ac6153e
Tom G. Christensen <tgc@jupiterrise.com>
2006-10-21 14:34:33 +0000

    Automake 1.10 for Irix 6.2

---
+++ automake110/build.sh
@@ -0,0 +1,60 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=automake
+version=1.10
+pkgver=1
+source[0]=$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+configure_args="$configure_args --program-suffix=-$version"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    # What where they thinking :(
+    ${RM} -f ${stagedir}${prefix}/${_bindir}/*-${version}-${version}
+    doc AUTHORS COPYING ChangeLog INSTALL NEWS README THANKS TODO
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
