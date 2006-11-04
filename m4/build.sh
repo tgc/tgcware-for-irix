befa2a1dad7d80a661aeadf0d7a314218b5f0a26
Tom G. Christensen <tgc@jupiterrise.com>
2006-11-04 12:53:46 +0000

    Update to 1.4.7

--- m4/build.sh
+++ m4/build.sh
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=m4
-version=1.4.6
-pkgver=2
+version=1.4.7
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
