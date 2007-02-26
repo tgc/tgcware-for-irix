7f6c0563a2bdd694f869a9e0880b719ddf78e218
Tom G. Christensen <tgc@jupiterrise.com>
2007-02-26 21:31:16 +0000

    Update to 4.1.2

--- gcc41/build.sh
+++ gcc41/build.sh
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gcc
-version=4.1.0
-pkgver=3
+version=4.1.2
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -27,12 +27,11 @@ make_build_target="bootstrap"
 gnuld="--with-gnu-ld --with-ld=/usr/tgcware/bin/gld"
 gnuas="--with-gnu-as --with-as=/usr/tgcware/bin/gas"
 langs="--with-gmp=/usr/tgcware --with-mpfr=/usr/tgcware --enable-languages=c,c++,fortran,objc,obj-c++"
-withada=1
-
+withada=0
+withjava=0
 gas=0
 gld=0
 
-objdir=cccfooa_gas
 
 global_config_args="--prefix=$prefix --with-local-prefix=$prefix --disable-nls --with-libiconv-prefix=/usr/tgcware"
 
@@ -43,15 +42,22 @@ if [ "$_os" = "irix53" ]; then
     gas=1
     gld=1
     withada=0
+    withjava=0
     objdir=cccfoo_gtools
     [ $withada -eq 1 ] && export GNAT_ROOT=$HOME/gcc-3.4.0-20040204-mips-sgi-irix5.3 # Location of gnatbind
     configure_args="$global_config_args --enable-shared=libstdc++ $gnuas $gnuld"
 fi
 if [ "$_os" = "irix62" ]; then
-    configure_args="$global_config_args --disable-multilib $gnuas --enable-shared=libstdc++"
+    #configure_args="$global_config_args --disable-multilib --enable-shared=libstdc++"
     #export CC='/usr/people/tgc/bin/cc -n32 -mips3'
-    export CC=/usr/tgcware/gcc-3.4.6/bin/gcc
-    export GNAT_ROOT=/usr/tgcware/gcc-3.4.6/bin
+    configure_args="$global_config_args --enable-shared=libstdc++"
+    export CC=/usr/tgcware/gcc-4.1.1/bin/gcc
+    export CONFIG_SHELL=/bin/ksh
+    withada=1
+    gas=1
+    objdir=cccfooa_gas
+    [ $withada -eq 1 ] && export GNAT_ROOT=/usr/tgcware/gcc-4.1.1/bin
+    [ $withjava -eq 1 ] && configure_args="$configure_args --with-system-zlib --enable-java-awt=gtk"
 fi
 
 if [ $gas -eq 1 -o $gld -eq 1 ]; then
@@ -59,11 +65,14 @@ if [ $gas -eq 1 -o $gld -eq 1 ]; then
    export NM=/usr/tgcware/bin/gnm
    export AR=/usr/tgcware/bin/gar
    export RANLIB=/usr/tgcware/bin/granlib
+   export OBJDUMP=/usr/tgcware/bin/gobjdump
    export NM_FOR_TARGET=/usr/tgcware/bin/gnm
    export AR_FOR_TARGET=/usr/tgcware/bin/gar
    export RANLIB_FOR_TARGET=/usr/tgcware/bin/granlib
+   export OBJDUMP_FOR_TARGET=/usr/tgcware/bin/gobjdump
 fi
 [ $withada -eq 1 ] && langs="$langs,ada"
+[ $withjava -eq 1 ] && langs="$langs,java"
 [ $gas -eq 1 ] && configure_args="$configure_args $gnuas"
 [ $gld -eq 1 ] && configure_args="$configure_args $gnuld"
 configure_args="$configure_args $langs"
@@ -101,37 +110,29 @@ install()
     $RM -f ${stagedir}${prefix}/${_infodir}/dir
     custom_install=1
     generic_install
-    gcclibdir=${stagedir}${prefix}/${_libdir}32
-    deflibdir=${stagedir}${defprefix}/${_libdir}
-    $RM -f ${gcclibdir}/*.la
+    $FIND ${stagedir} -name '*.la' -print | $XARGS $RM -f
 
-    ############### begin
-    # This should be handled in a seperate gcc runtime package
-    # to avoid deps on the versioned gcc package
-    #$MKDIR -p ${deflibdir}
-    #$MV ${gcclibdir}/libstdc++.so.* ${deflibdir}
-    #$MV ${gcclibdir}/libgcc_s.so.1 ${deflibdir}/libgcc_s-${version}.so.1
-    #setdir ${gcclibdir}
-    #$LN -sf ../../${_libdir}/libstdc++.so.7.7 libstdc++.so
-    #$LN -sf ../../${_libdir}/libgcc_s-${version}.so.1 libgcc_s.so
-    #$LN -sf ../../${_libdir}/libgcc_s.so.1 libgcc_s.so
-    #setdir ${deflibdir}
-    #$LN -sf libgcc_s-${version}.so.1 libgcc_s.so.1
-    #################### end
-    
     # Turn all the hardlinks in bin into symlinks
     setdir ${stagedir}${prefix}/${_bindir}
     for i in c++ mips-sgi-${os}-c++ mips-sgi-${os}-g++
     do
-	$LN -sf g++ $i
+	$LN -sf ${prefix}/${_bindir}/g++ $i
     done
     for i in mips-sgi-${os}-gcc mips-sgi-${os}-gcc-$version
     do
-	$LN -sf gcc $i
+	$LN -sf ${prefix}/${_bindir}/gcc $i
     done
-    $LN -sf gfortran mips-sgi-${os}-gfortran
+    $LN -sf ${prefix}/${_bindir}/gfortran mips-sgi-${os}-gfortran
     doc COPYING* BUGS FAQ MAINTAINERS NEWS
     $MV ${stagedir}${prefix}/${_sharedir} ${stagedir}${lprefix}
+}
+
+reg check
+check()
+{
+    setdir source
+    setdir ../$objdir
+    $MAKE_PROG -k check
 }
 
 reg pack
