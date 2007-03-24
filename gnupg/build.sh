02fe3144dfa9f1a5eb3ea8830a6694da535dbe95
Tom G. Christensen <tgc@jupiterrise.com>
2007-03-24 19:59:07 +0000

    Update to 1.4.7

--- gnupg/build.sh
+++ gnupg/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gnupg
-version=1.4.6
+version=1.4.7
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
