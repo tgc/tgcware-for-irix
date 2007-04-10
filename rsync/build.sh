e61533495cb53ca9fbb3a8ba40f6989740281311
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-10 19:05:50 +0000

    Update to 2.6.9

--- rsync/build.sh
+++ rsync/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=rsync
-version=2.6.8
+version=2.6.9
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -21,8 +21,11 @@ source[0]=$topdir-$version.tar.gz
 # Global settings
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 export CPPFLAGS="-I/usr/tgcware/include"
-configure_args='--prefix=${prefix} --with-rsyncd-conf=${prefix}/${_sysconfdir}/rsyncd.conf --disable-ipv6'
+configure_args='--prefix=${prefix} --mandir=${prefix}/${_mandir} --with-rsyncd-conf=${prefix}/${_sysconfdir}/rsyncd.conf --disable-ipv6'
 [ "$_os" = "irix62" ] && ac_overrides="ac_cv_func_inet_pton=no ac_cv_func_inet_ntop=no"
+
+[ "$_os" = "irix53" ] && export CC=cc && mipspro=2
+[ "$_os" = "irix62" ] && export CC=cc && mipspro=1
 
 reg prep
 prep()
