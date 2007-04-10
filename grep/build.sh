6247b0ad24d8def5a527a9c48a94270bdfcd1c41
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-10 18:31:35 +0000

    Rebuild without bogus deps

--- grep/build.sh
+++ grep/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=grep
 version=2.5.1a
-pkgver=3
+pkgver=4
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -21,7 +21,7 @@ source[0]=$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-set_configure_args '--prefix=$prefix --enable-nls --with-libiconv-prefix=/usr/tgcware'
+configure_args='--prefix=$prefix --enable-nls --with-libiconv-prefix=/usr/tgcware'
 
 reg prep
 prep()
