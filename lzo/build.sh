45b370d3c8e20e198e17ba1b763ab23d3390e5d7
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-13 13:02:38 +0000

    Rebuild to fix bogus deps

--- lzo/build.sh
+++ lzo/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=lzo
 version=2.02
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -22,6 +22,7 @@ source[0]=$topdir-$version.tar.gz
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 configure_args="$configure_args --enable-shared"
+[ "$_os" = "irix62" ] && export CC=cc && mipspro=1
 
 reg prep
 prep()
