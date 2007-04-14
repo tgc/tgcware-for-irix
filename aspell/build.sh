ae792f8c9d4e24657dfc8b3a1af5cb0722894892
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-14 15:36:36 +0000

    Update to 0.60.5

--- aspell/build.sh
+++ aspell/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=aspell
-version=0.60.4
+version=0.60.5
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -22,7 +22,7 @@ patch[1]=aspell-0.60.2-includes.patch
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args='--prefix=$prefix --enable-docdir=$prefix/${_vdocdir} --disable-nls'
+configure_args='--prefix=$prefix --mandir=${prefix}/${_mandir} --infodir=${prefix}/${_infodir} --enable-docdir=$prefix/${_vdocdir}'
 
 reg prep
 prep()
