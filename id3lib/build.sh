9d925bc96e010d745017be0fc83a84b3106504b8
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-15 09:06:04 +0000

    Rebuild to fix bogus deps

--- id3lib/build.sh
+++ id3lib/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=id3lib
 version=3.8.3
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
