ea3ed7008e3ecb41fb3c0f195bf096bd26058bdf
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-15 08:49:12 +0000

    Rebuild to fix bogus deps

--- openldap23/build.sh
+++ openldap23/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=openldap
 version=2.3.21
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tgz
 # If there are no patches, simply comment this
 patch[0]=openldap-2.3.21-find-needs-print.patch
@@ -24,7 +24,7 @@ patch[0]=openldap-2.3.21-find-needs-print.patch
 export CC=gcc
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args='--prefix=$prefix --disable-slapd --disable-slurpd'
+configure_args='--prefix=$prefix --mandir=${prefix}/${_mandir} --infodir=${prefix}/${_infodir} --disable-slapd --disable-slurpd'
 [ "$_os" = "irix62" ] && ac_overrides="ac_cv_func_inet_ntop=no"
 
 reg prep
