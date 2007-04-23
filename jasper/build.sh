49811a7122eb05774b1fe3b07c42cea58414cd6b
Tom G. Christensen <tgc@jupiterrise.com>
2007-04-23 19:04:37 +0000

    Update to 1.900.1 Add manpages

--- jasper/build.sh
+++ jasper/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=jasper
-version=1.701.0
-pkgver=2
+version=1.900.1
+pkgver=1
 source[0]=$topdir-$version.zip
 # If there are no patches, simply comment this
 #patch[0]=
