c45f6361c6528c6008833f7f21f77f31c5461299
Tom G. Christensen <tgc@jupiterrise.com>
2007-05-02 17:26:59 +0000

    Rebuild to fix bogus deps

--- ghostscript/build.sh
+++ ghostscript/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=ghostscript
 version=8.15
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -21,6 +21,7 @@ source[0]=$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="--prefix=$prefix"
 
 shortroot=1
 
