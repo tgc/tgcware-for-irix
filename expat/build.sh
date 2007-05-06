065136b1f15230f50b47c70cc63c6819086deaeb
Tom G. Christensen <tgc@jupiterrise.com>
2007-05-06 15:38:00 +0000

    range was insufficient in replaces file

--- expat/build.sh
+++ expat/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=expat
 version=2.0.0
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
