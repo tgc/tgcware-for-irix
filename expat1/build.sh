0d5424b3a047bec6ceb90af5b5eef302386e874c
Tom G. Christensen <tgc@jupiterrise.com>
2007-05-06 15:40:25 +0000

    expat 1.95.8 expat was already upgraded to 2.0 and I needed a new build of 1.95.8 to replace the one with broken deps that apr needs

---
+++ expat1/build.sh
@@ -0,0 +1,61 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=expat
+version=1.95.8
+pkgver=4
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="--prefix=$prefix"
+
+shortroot=1
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install prefix
+    doc README Changes COPYING doc/reference.html doc/*.png doc/*.css
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
