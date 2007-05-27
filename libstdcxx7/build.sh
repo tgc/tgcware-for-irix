35573cc046d04cd79c24ee41c5fbceec2edda20f
Tom G. Christensen <tgc@jupiterrise.com>
2007-05-27 18:10:46 +0000

    Update to libstdc++ fom gcc 4.2.0

--- libstdcxx7/build.sh
+++ libstdcxx7/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=libstdcxx7
 version=7
-pkgver=4
+pkgver=5
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -18,7 +18,7 @@ pkgver=4
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-gcc=4.1.2
+gcc=4.2.0
 [ "$_os" = "irix53" ] && gcc=3.4.6
 
 reg prep
@@ -36,22 +36,10 @@ build()
 reg install
 install()
 {
-    if [ "$gcc" = "4.1.2" ]; then
-	echo "BuildRequires: tgc_gcc412.sw.base"
-	clean stage
-	$MKDIR -p ${stagedir}${prefix}/$_libdir
-	$CP -pr $prefix/gcc-4.1.2/${_libdir}32/libstdc++.so.7.8 ${stagedir}${prefix}/$_libdir
-	setdir ${stagedir}${prefix}/${_libdir}
-	$LN -sf libstdc++.so.7.8 libstdc++.so.7
-    fi
-    if [ "$gcc" = "3.4.6" ]; then
-	echo "BuildRequires: tgc_gcc346.sw.base"
-	clean stage
-	$MKDIR -p ${stagedir}${prefix}/$_libdir
-	$CP -pr $prefix/gcc-$gcc/${_libdir}/libstdc++.so.7.3 ${stagedir}${prefix}/$_libdir
-	setdir ${stagedir}${prefix}/${_libdir}
-	$LN -sf libstdc++.so.7.3 libstdc++.so.7
-    fi
+    echo "BuildRequires: tgc_gcc $gcc"
+    clean stage
+    $MKDIR -p ${stagedir}${prefix}/$_libdir
+    $CP -pR $prefix/gcc-$gcc/${_libdir}32/libstdc++.so.7* ${stagedir}${prefix}/$_libdir
 }
 
 reg pack
