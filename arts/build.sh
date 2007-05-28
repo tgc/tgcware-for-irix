5021450e86e43d0930cd992c80dd5f8bd8205a5a
Tom G. Christensen <tgc@jupiterrise.com>
2007-05-28 10:20:52 +0000

    Update to 1.5.7

--- arts/build.sh
+++ arts/build.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/tgcware/bin/bash
 #
 # This is a generic build.sh script
 # It can be used nearly unmodified with many packages
@@ -9,23 +9,25 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=arts
-version=1.4.0
-pkgver=2
+version=1.5.7
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-patch[0]=arts-1.4.0-sgiaudio.patch
+patch[0]=arts-1.5.7-oldapi.patch
 patch[1]=arts-1.4.0-oldaudio.patch
-patch[2]=arts-1.4.0-srandom.patch
-patch[3]=arts-1.4.0-socklen_t.patch
+patch[2]=arts-1.5.7-srandom.patch
+patch[3]=arts-1.5.7-socklen_t.patch
 patch[4]=arts-1.4.0-sched.patch
+patch[5]=arts-1.5.0-check_tmp_dir.patch
+patch[6]=arts-1.5.0-assert.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-export CPPFLAGS="-I/usr/local/include -I/usr/local/qt-3.3/include"
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib -L/usr/local/qt-3.3/lib -Wl,-rpath,/usr/local/qt-3.3/lib"
-set_configure_args '--prefix=$prefix --disable-rpath'
+export CPPFLAGS="-I/usr/tgcware/include -I/usr/tgcware/qt-3.3/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib -L/usr/tgcware/qt-3.3/lib -Wl,-rpath,/usr/tgcware/qt-3.3/lib"
+configure_args='--prefix=$prefix --enable-libsuffix= --disable-rpath --disable-pie --with-extra-includes=/usr/tgcware/include --with-extra-libs=/usr/tgcware/lib'
 
 autonuke=0
 
