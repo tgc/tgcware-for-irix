0de6fce57953eeb48137191f16b0d43d20c4a207
Tom G. Christensen <tgc@jupiterrise.com>
2007-06-09 09:31:11 +0000

    Rebuild to put manpages in the right place

--- autoconf260/build.sh
+++ autoconf260/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=autoconf
 version=2.60
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
