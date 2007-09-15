d2f7b465b758e0fb6824d01b5cfad4a349a6451a
Tom G. Christensen <tgc@jupiterrise.com>
2007-09-15 14:58:14 +0000

    Update to 2.18

--- binutils/build.sh
+++ binutils/build.sh
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=binutils
-version=2.16.1
-pkgver=2
+version=2.18
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -19,8 +19,10 @@ source[0]=$topdir-$version.tar.bz2
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 export CONFIG_SHELL=/bin/ksh
-configure_args="--prefix=$prefix --program-prefix=g"
+configure_args="--prefix=$prefix --program-prefix=g --disable-nls"
 
 reg prep
 prep()
@@ -38,12 +40,16 @@ reg install
 install()
 {
     generic_install DESTDIR
-#    $RM -f ${stagedir}${prefix}/${_bindir}/ld
-#    $RM -f ${stagedir}${prefix}/${_infodir}/dir
-#    $RM -f ${stagedir}${prefix}/${_bindir}/c++filt
-#    $RM -f ${stagedir}${prefix}/${_libdir}/*.la
     $RM -f ${stagedir}${prefix}/${_mandir}/man1/g{dlltool,nlmconv,windres}*
-    doc README
+    doc COPYING*
+    setdir ${stagedir}${prefix}/${_libdir}
+    ${__mv} ../lib32/*.a .
+    ${__rmdir} ../lib32
+    # Convert hardlinks to symlinks
+    setdir ${stagedir}${prefix}/${_bindir}
+    for p in ar as ld nm objcopy objdump ranlib strip; do
+	${__ln} -sf ${prefix}/mips-sgi-${os}/bin/$p g$p
+    done
 }
 
 reg pack
