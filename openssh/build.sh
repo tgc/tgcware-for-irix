0c2f7d9795f6d01f68097466e19e62003bfeb1f8
Tom G. Christensen <tgc@jupiterrise.com>
2007-09-24 17:49:16 +0000

    Update to 4.7p1

--- openssh/build.sh
+++ openssh/build.sh
@@ -9,7 +9,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssh
-version=4.6p1
+version=4.7p1
 pkgver=1
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
