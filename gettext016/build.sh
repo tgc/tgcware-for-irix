e6fd875c7265ece9f11469ebadc97ddb3e729568
Tom G. Christensen <tgc@jupiterrise.com>
2007-10-20 13:28:41 +0000

    Rebuild with -no_rqs enabled gcc specs on irix 5.3

--- gettext016/build.sh
+++ gettext016/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=gettext
 version=0.16.1
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
