bc5f4b036cf1c9092e0e02f7b919e61858e75efb
Tom G. Christensen <tgc@jupiterrise.com>
2007-11-07 21:03:00 +0000

    Rebuild for 5.3

--- dillo/build.sh
+++ dillo/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=dillo
 version=0.8.6
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=dillo-0.8.6-linkorder.patch
@@ -22,6 +22,7 @@ patch[0]=dillo-0.8.6-linkorder.patch
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 configure_args="$configure_args --disable-dlgui"
+[ "$_os" = "irix53" ] && configure_args="$configure_args --disable-threaded-dns"
 
 reg prep
 prep()
@@ -39,7 +40,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    ${GSED} -i 's|/usr/bin/perl|/usr/tgcware/bin/perl|' ${stagedir}${prefix}/${_bindir}/dpidc
+    ${__gsed} -i 's|/usr/bin/perl|/usr/tgcware/bin/perl|' ${stagedir}${prefix}/${_bindir}/dpidc
     doc ChangeLog COPYING AUTHORS README
 }
 
