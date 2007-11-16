1ff8a565cc453df1a7ac53f4eeaf73f6fc8ddcbd
Tom G. Christensen <tgc@jupiterrise.com>
2007-11-16 16:57:35 +0000

    libobjc2 for Irix 5.3

---
+++ libobjc2/build.sh
@@ -0,0 +1,67 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libobjc2
+version=1
+pkgver=1
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+gcc=4.2.2
+[ "$_os" = "irix53" ] && gcc=3.4.6
+version=$gcc
+
+reg prep
+prep()
+{
+    : nothing to prep
+}
+
+reg build
+build()
+{
+    : nothing to build
+}
+
+reg install
+install()
+{
+    echo "BuildRequires: tgc_gcc $gcc"
+    clean stage
+    ${__mkdir} -p ${stagedir}${prefix}/$_libdir
+    [ "$_os" = "irix62" ] && libsuffix=32
+    (cd $prefix/gcc-$gcc/${_libdir}${libsuffix}; ${__tar} -cf - libobjc.so.2*) |
+    (cd ${stagedir}${prefix}/${_libdir}; ${__tar} -xvpf -)
+    
+    # fix up relnotes
+    ${__sed} -e "s/@@GCCVER@@/$gcc/" ${metadir}/relnotes > ${metadir}/relnotes.$_os.txt
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    META_CLEAN="$META_CLEAN relnotes.$_os.txt"
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
