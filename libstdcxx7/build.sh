aae03c745de977b756b41d4e3713ca579f989156
Tom G. Christensen <tgc@jupiterrise.com>
2007-11-16 16:52:49 +0000

    Update to match libgcc_s1 except for versioning. I'd really like to change the versioning to match but it'll require a new package name and support for the updates keyword in the specfile.

--- libstdcxx7/build.sh
+++ libstdcxx7/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=libstdcxx7
 version=7
-pkgver=5
+pkgver=6
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -18,7 +18,7 @@ pkgver=5
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-gcc=4.2.0
+gcc=4.2.2
 [ "$_os" = "irix53" ] && gcc=3.4.6
 
 reg prep
@@ -38,13 +38,19 @@ install()
 {
     echo "BuildRequires: tgc_gcc $gcc"
     clean stage
-    $MKDIR -p ${stagedir}${prefix}/$_libdir
-    $CP -pR $prefix/gcc-$gcc/${_libdir}32/libstdc++.so.7* ${stagedir}${prefix}/$_libdir
+    ${__mkdir} -p ${stagedir}${prefix}/$_libdir
+    [ "$_os" = "irix62" ] && libsuffix=32
+    (cd $prefix/gcc-$gcc/${_libdir}${libsuffix}; ${__tar} -cf - libstdc++.so.7*) |
+    (cd ${stagedir}${prefix}/${_libdir}; ${__tar} -xvpf -)
+
+    # fix up relnotes
+    ${__sed} -e "s/@@GCCVER@@/$gcc/" ${metadir}/relnotes > ${metadir}/relnotes.$_os.txt
 }
 
 reg pack
 pack()
 {
+    META_CLEAN="$META_CLEAN relnotes.$_os.txt"
     generic_pack
 }
 
