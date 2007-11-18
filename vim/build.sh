bc3a2efc7a1bc4a27196dc4085b58896478b7d8e
Tom G. Christensen <tgc@jupiterrise.com>
2007-11-18 19:38:29 +0000

    Update to 7.1p156

--- vim/build.sh
+++ vim/build.sh
@@ -9,216 +9,145 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=vim
-version=7.0
-pkgver=4
-source[0]=$topdir-7.0.tar.bz2
+version=7.1
+pkgver=1
+source[0]=$topdir-7.1.tar.bz2
 # If there are no patches, simply comment this
 # Generate patchlist like this:
-# num=0; for i in `grep -v Win32 README  |grep -v \(extra|awk '{ print $2 }'|grep 7.0`; do echo "patch[$num]=$i"; let num=num+1; done
-patch[0]=7.0.001
-patch[1]=7.0.002
-patch[2]=7.0.003
-patch[3]=7.0.004
-patch[4]=7.0.006
-patch[5]=7.0.007
-patch[6]=7.0.008
-patch[7]=7.0.009
-patch[8]=7.0.010
-patch[9]=7.0.011
-patch[10]=7.0.012
-patch[11]=7.0.013
-patch[12]=7.0.014
-patch[13]=7.0.015
-patch[14]=7.0.016
-patch[15]=7.0.017
-patch[16]=7.0.018
-patch[17]=7.0.019
-patch[18]=7.0.020
-patch[19]=7.0.021
-patch[20]=7.0.022
-patch[21]=7.0.023
-patch[22]=7.0.024
-patch[23]=7.0.025
-patch[24]=7.0.026
-patch[25]=7.0.029
-patch[26]=7.0.030
-patch[27]=7.0.031
-patch[28]=7.0.033
-patch[29]=7.0.034
-patch[30]=7.0.035
-patch[31]=7.0.036
-patch[32]=7.0.037
-patch[33]=7.0.038
-patch[34]=7.0.039
-patch[35]=7.0.040
-patch[36]=7.0.041
-patch[37]=7.0.042
-patch[38]=7.0.043
-patch[39]=7.0.044
-patch[40]=7.0.046
-patch[41]=7.0.047
-patch[42]=7.0.048
-patch[43]=7.0.049
-patch[44]=7.0.050
-patch[45]=7.0.051
-patch[46]=7.0.052
-patch[47]=7.0.053
-patch[48]=7.0.054
-patch[49]=7.0.055
-patch[50]=7.0.056
-patch[51]=7.0.058
-patch[52]=7.0.059
-patch[53]=7.0.060
-patch[54]=7.0.061
-patch[55]=7.0.062
-patch[56]=7.0.063
-patch[57]=7.0.064
-patch[58]=7.0.066
-patch[59]=7.0.067
-patch[60]=7.0.068
-patch[61]=7.0.069
-patch[62]=7.0.070
-patch[63]=7.0.071
-patch[64]=7.0.072
-patch[65]=7.0.073
-patch[66]=7.0.075
-patch[67]=7.0.076
-patch[68]=7.0.077
-patch[69]=7.0.078
-patch[70]=7.0.079
-patch[71]=7.0.080
-patch[72]=7.0.081
-patch[73]=7.0.082
-patch[74]=7.0.083
-patch[75]=7.0.084
-patch[76]=7.0.085
-patch[77]=7.0.086
-patch[78]=7.0.087
-patch[79]=7.0.088
-patch[80]=7.0.089
-patch[81]=7.0.090
-patch[82]=7.0.091
-patch[83]=7.0.092
-patch[84]=7.0.093
-patch[85]=7.0.094
-patch[86]=7.0.095
-patch[87]=7.0.096
-patch[88]=7.0.097
-patch[89]=7.0.098
-patch[90]=7.0.099
-patch[91]=7.0.100
-patch[92]=7.0.101
-patch[93]=7.0.102
-patch[94]=7.0.103
-patch[95]=7.0.104
-patch[96]=7.0.105
-patch[97]=7.0.106
-patch[98]=7.0.107
-patch[99]=7.0.109
-patch[100]=7.0.110
-patch[101]=7.0.111
-patch[102]=7.0.112
-patch[103]=7.0.113
-patch[104]=7.0.114
-patch[105]=7.0.115
-patch[106]=7.0.116
-patch[107]=7.0.117
-patch[108]=7.0.118
-patch[109]=7.0.119
-patch[110]=7.0.120
-patch[111]=7.0.121
-patch[112]=7.0.122
-patch[113]=7.0.123
-patch[114]=7.0.124
-patch[115]=7.0.125
-patch[116]=7.0.126
-patch[117]=7.0.127
-patch[118]=7.0.128
-patch[119]=7.0.129
-patch[120]= #7.0.132 - improperly tagged - depends on 130 which is (extra)
-patch[121]=7.0.133
-patch[122]=7.0.134
-patch[123]=7.0.135
-patch[124]=7.0.136
-patch[125]=7.0.137
-patch[126]=7.0.139
-patch[127]=7.0.140
-patch[128]=7.0.141
-patch[129]=7.0.142
-patch[130]=7.0.143
-patch[131]=7.0.144
-patch[132]=7.0.145
-patch[133]=7.0.146
-patch[134]=7.0.147
-patch[135]=7.0.148
-patch[136]=7.0.149
-patch[137]=7.0.150
-patch[138]=7.0.151
-patch[139]=7.0.152
-patch[140]=7.0.153
-patch[141]=7.0.154
-patch[142]=7.0.155
-patch[143]=7.0.157
-patch[144]=7.0.158
-patch[145]=7.0.159
-patch[146]=7.0.160
-patch[147]=7.0.162
-patch[148]=7.0.163
-patch[149]=7.0.164
-patch[150]=7.0.165
-patch[151]=7.0.166
-patch[152]=7.0.167
-patch[153]=7.0.168
-patch[154]=7.0.169
-patch[155]=7.0.172
-patch[156]=7.0.173
-patch[157]=7.0.174
-patch[158]=7.0.175
-patch[159]=7.0.176
-patch[160]=7.0.177
-patch[161]=7.0.178
-patch[162]=7.0.179
-patch[163]=7.0.181
-patch[164]=7.0.182
-patch[165]=7.0.183
-patch[166]=7.0.184
-patch[167]=7.0.185
-patch[168]=7.0.186
-patch[169]=7.0.187
-patch[170]=7.0.188
-patch[171]=7.0.189
-patch[172]=7.0.190
-patch[173]=7.0.191
-patch[174]=7.0.192
-patch[175]=7.0.193
-patch[176]=7.0.194
-patch[177]=7.0.195
-patch[178]=7.0.196
-patch[179]=7.0.199
-patch[180]=7.0.200
-patch[181]=7.0.201
-patch[182]=7.0.202
-patch[183]=7.0.203
-patch[184]=7.0.204
-patch[185]=7.0.205
-patch[186]=7.0.206
-patch[187]=7.0.207
-patch[188]= #7.0.208 - VMS only
-patch[189]=7.0.209
-patch[190]=7.0.210
-patch[191]=7.0.211
-patch[192]=7.0.212
-patch[193]=7.0.213
-patch[194]=7.0.214
-patch[195]=7.0.216
-patch[196]=7.0.217
-patch[197]=7.0.218
-patch[198]=7.0.219
-patch[199]=7.0.220
-patch[200]=7.0.221
-patch[201]=7.0.222
-patch[202]=7.0.223
-patch[203]=7.0.224
+# num=0; for i in `grep -v Win32 README  |grep -v \(extra|awk '{ print $2 }'|grep 7.1`; do echo "patch[$num]=$i"; let num=num+1; done
+patch[0]=7.1.001
+patch[1]=7.1.002
+patch[2]=7.1.004
+patch[3]=7.1.005
+patch[4]=7.1.006
+patch[5]=7.1.008
+patch[6]=7.1.009
+patch[7]=7.1.010
+patch[8]=7.1.011
+patch[9]=7.1.012
+patch[10]=7.1.013
+patch[11]=7.1.014
+patch[12]=7.1.015
+patch[13]=7.1.016
+patch[14]=7.1.017
+patch[15]=7.1.018
+patch[16]=7.1.019
+patch[17]=7.1.020
+patch[18]=7.1.022
+patch[19]=7.1.023
+patch[20]=7.1.024
+patch[21]=7.1.025
+patch[22]=7.1.026
+patch[23]=7.1.027
+patch[24]=7.1.028
+patch[25]=7.1.029
+patch[26]=7.1.030
+patch[27]=7.1.031
+patch[28]=7.1.032
+patch[29]=7.1.033
+patch[30]=7.1.034
+patch[31]=7.1.035
+patch[32]=7.1.036
+patch[33]=7.1.037
+patch[34]=7.1.038
+patch[35]=7.1.039
+patch[36]=7.1.040
+patch[37]=7.1.042
+patch[38]=7.1.043
+patch[39]=7.1.044
+patch[40]=7.1.045
+patch[41]=7.1.046
+patch[42]=7.1.047
+patch[43]=7.1.048
+patch[44]=7.1.049
+patch[45]=7.1.050
+patch[46]=7.1.051
+patch[47]=7.1.052
+patch[48]=7.1.053
+patch[49]=7.1.054
+patch[50]=7.1.055
+patch[51]=7.1.056
+patch[52]=7.1.057
+patch[53]=7.1.058
+patch[54]=7.1.059
+patch[55]=7.1.060
+patch[56]=7.1.061
+patch[57]=7.1.062
+patch[58]=7.1.063
+patch[59]=7.1.064
+patch[60]=7.1.066
+patch[61]=7.1.067
+patch[62]=7.1.068
+patch[63]=7.1.069
+patch[64]=7.1.071
+patch[65]=7.1.073
+patch[66]=7.1.074
+patch[67]=7.1.075
+patch[68]=7.1.076
+patch[69]=7.1.078
+patch[70]=7.1.079
+patch[71]=7.1.081
+patch[72]=7.1.082
+patch[73]=7.1.083
+patch[74]=7.1.084
+patch[75]=7.1.085
+patch[76]=7.1.086
+patch[77]=7.1.087
+patch[78]=7.1.089
+patch[79]=7.1.090
+patch[80]=7.1.093
+patch[81]=7.1.094
+patch[82]=7.1.095
+patch[83]=7.1.096
+patch[84]=7.1.097
+patch[85]=7.1.098
+patch[86]=7.1.099
+patch[87]=7.1.101
+patch[88]=7.1.102
+patch[89]=7.1.103
+patch[90]=7.1.104
+patch[91]=7.1.105
+patch[92]=7.1.106
+patch[93]=7.1.107
+patch[94]=7.1.109
+patch[95]=7.1.111
+patch[96]=7.1.112
+patch[97]=7.1.113
+patch[98]=7.1.114
+patch[99]=7.1.115
+patch[100]=7.1.116
+patch[101]=7.1.117
+patch[102]=7.1.118
+patch[103]=7.1.119
+patch[104]=7.1.120
+patch[105]=7.1.121
+patch[106]=7.1.122
+patch[107]=7.1.125
+patch[108]=7.1.127
+patch[109]=7.1.130
+patch[110]=7.1.131
+patch[111]=7.1.132
+patch[112]=7.1.133
+patch[113]=7.1.136
+patch[114]=7.1.137
+patch[115]=7.1.138
+patch[116]=7.1.139
+patch[117]=7.1.140
+patch[118]=7.1.141
+patch[119]=7.1.142
+patch[120]=7.1.143
+patch[121]=7.1.144
+patch[122]=7.1.145
+patch[123]=7.1.147
+patch[124]=7.1.148
+patch[125]=7.1.149
+patch[126]=7.1.150
+patch[127]=7.1.151
+patch[128]=7.1.152
+patch[129]=7.1.153
+patch[130]=7.1.154
+patch[131]=7.1.155
+patch[132]=7.1.156
 
 # Helper var
 patchcount=${#patch[@]}
@@ -228,20 +157,23 @@ patchcount=${#patch[@]}
 
 # Global settings
 # We need to override this
-topsrcdir=vim70
+topsrcdir=vim71
 patchdir=$srcfiles/vim-${version}-patches
 patch_prefix="-p0"
-export CFLAGS="-I/usr/tgcware/include"
-export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+export CC=cc
+export CPPFLAGS="-I/usr/tgcware/include"
 # What gui should we build?
 gui=motif
 configure_args='--prefix=$prefix --enable-gui=$gui --with-features=huge --enable-multibyte --disable-perlinterp --disable-pythoninterp --disable-tclinterp --with-compiledby="<irixpkg@jupiterrise.com>" --disable-netbeans'
 configlog=src/auto/config.log
 
-export CC=cc
-export CFLAGS=-O2
 [ "$_os" = "irix62" ] && mipspro=1
-[ "$_os" = "irix53" ] && mipspro=2
+if [ "$_os" = "irix53" ]; then
+    mipspro=2
+    NO_RQS="-Wl,-no_rqs"
+fi
+
+export LDFLAGS="$NO_RQS -L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 
 # Custom subsystems...
 subsysconf=$metadir/subsys.conf
@@ -263,9 +195,9 @@ build()
     generic_build
     # Save the gui binary for later
     setdir source
-    ${CP} src/vim src/gvim
+    ${__cp} src/vim src/gvim
     setdir source
-    $MAKE_PROG clean
+    ${__make} clean
     gui="no --with-x=no"
     generic_build
 }
@@ -275,21 +207,21 @@ install()
 {
     generic_install DESTDIR
     setdir source
-    ${CP} src/gvim ${stagedir}${prefix}/${_bindir}
+    ${__cp} src/gvim ${stagedir}${prefix}/${_bindir}
     setdir ${stagedir}${prefix}/${_bindir}
-    ${LN} -s gvim gvimdiff
-    ${LN} -s gvim gview
+    ${__ln} -s gvim gvimdiff
+    ${__ln} -s gvim gview
     setdir ${stagedir}${prefix}/${_mandir}/man1
-    ${LN} -s vim.1 gvim.1
-    ${LN} -s vim.1 gview.1
-    ${LN} -s vimdiff.1 gvimdiff.1
+    ${__ln} -s vim.1 gvim.1
+    ${__ln} -s vim.1 gview.1
+    ${__ln} -s vimdiff.1 gvimdiff.1
     custom_install=1
     generic_install DESTDIR
     doc README.txt
-    setdir ${stagedir}${prefix}/${_sharedir}/vim/vim70/lang/
-    ${MV} "menu_chinese(gb)_gb.936.vim" "menu_chinese_gb__gb.936.vim"
-    ${MV} "menu_chinese(taiwan)_taiwan.950.vim" "menu_chinese_taiwan__taiwan.950.vim"
-    $RM -rf ${stagedir}${prefix}/${_mandir}/{fr,it,pl,ru}*
+    setdir ${stagedir}${prefix}/${_sharedir}/vim/vim71/lang/
+    ${__mv} "menu_chinese(gb)_gb.936.vim" "menu_chinese_gb__gb.936.vim"
+    ${__mv} "menu_chinese(taiwan)_taiwan.950.vim" "menu_chinese_taiwan__taiwan.950.vim"
+    ${__rm} -rf ${stagedir}${prefix}/${_mandir}/{fr,it,pl,ru}*
 }
 
 reg pack
