35778af251d0314c386aac3d7f1888b10cfb3ed3
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-18 21:38:00 +0000

    Small cleanup and rebuild for -no_rqs on Irix 5.3

--- cups/build.sh
+++ cups/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=cups
 version=1.1.23
-pkgver=3
+pkgver=4
 source[0]=$topdir-$version-source.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=cups-1.1.23-pkg.patch
@@ -25,8 +25,6 @@ export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 export DSOFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 
-catman=0
-gzman=0
 topinstalldir=/
 pkgdefprefix=usr/tgcware
 
@@ -48,14 +46,16 @@ install()
 {
     generic_install BUILDROOT
     doc CHANGES.txt CGI.txt ENCRYPTION.txt LICENSE.txt README.txt
-    ${RM} -rf ${stagedir}${prefix}/${_mandir}/man*
-    ${RM} -rf ${stagedir}${prefix}/${_mandir}/*/man*
+    ${__rm} -rf ${stagedir}${prefix}/${_mandir}/man*
+    ${__rm} -rf ${stagedir}${prefix}/${_mandir}/*/man*
 
     # Make cups available when doing chkconfig list and don't start automatically
-    ${MKDIR} -p ${stagedir}/${_sysconfdir}/config
+    ${__mkdir} -p ${stagedir}/${_sysconfdir}/config
     echo "off" >${stagedir}/${_sysconfdir}/config/cups
 
-    $GSED -i 's|${datarootdir}|/usr/tgcware/share|g' ${stagedir}${prefix}/${_sysconfdir}/cups/cupsd.conf
+    echo "${_sysconfdir}/config/cups config(noupdate)" > $metadir/ops
+
+    ${__gsed} -i 's|${datarootdir}|/usr/tgcware/share|g' ${stagedir}${prefix}/${_sysconfdir}/cups/cupsd.conf
 }
 
 reg pack
@@ -67,6 +67,7 @@ pack()
 reg distclean
 distclean()
 {
+    META_CLEAN="$META_CLEAN ops"
     clean distclean
 }
 
