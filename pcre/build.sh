b0c4154fbe7f409172a27cb3fad1e9e383300bbb
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-18 18:39:02 +0000

    Update to 7.5

--- pcre/build.sh
+++ pcre/build.sh
@@ -9,8 +9,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=pcre
-version=7.4
-pkgver=2
+version=7.5
+pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -21,6 +21,7 @@ source[0]=$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --enable-utf8"
 
 reg prep
 prep()
