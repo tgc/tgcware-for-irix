a09805a32bf1c09bebde61d0860538c136382195
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-18 16:40:58 +0000

    Rebuild for -no_rqs on Irix 5.3.

--- libmng/build.sh
+++ libmng/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=libmng
 version=1.0.9
-pkgver=3
+pkgver=4
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
