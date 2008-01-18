df46d4b8b5568d75cc9342d0ede9b5c9a9695cc6
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-18 21:49:31 +0000

    gettext 0.17. This is the first package using the new 'updates' support.

---
+++ gettext017/build.sh
@@ -0,0 +1,64 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gettext
+version=0.17
+pkgver=5
+source[0]=$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --with-libiconv-prefix=/usr/tgcware --disable-rpath --disable-java --disable-threads --disable-openmp"
+ac_overrides="ac_cv_func_inet_ntop=no"
+
+# Don't add deps on previous releases
+ignore_deps="tgc_gettext016"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc NEWS README COPYING
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
