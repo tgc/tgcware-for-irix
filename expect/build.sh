ff9960c0e75814de8449546f944c613050becd6c
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-19 15:19:46 +0000

    Rebuild for -no_rqs on Irix 5.3.
    Also because tcl/tk is built with cc so must expect be or major makefile
    hackery would be needed. As it is the hackerey is now in build.sh to make
    sure the right params are used accordingly to how tcl was built.
    The -no-rpath patch simply nukes any attempts to include an rpath with the
    builddir in the finished binary.

--- expect/build.sh
+++ expect/build.sh
@@ -10,19 +10,30 @@
 # Check the following 4 variables before running the script
 topdir=expect
 version=5.43.0
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=expect-5.43-no-rpath.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
+export CC=$(_upls $(${__sed} -n '/^TCL_CC=/s/.*=//p' /usr/tgcware/lib/tclConfig.sh))
+if [ "$CC" = "cc" ]; then # tcl was built with cc
+    mipspro=1
+    # Can't include both inttypes.h and sys/types.h on 5.3 with cc
+    [ "$_os" = "irix53" ] && ac_overrides="ac_cv_header_inttypes_h=no"
+    [ "$_os" = "irix53" ] && NO_RQS="-Wl,-no_rqs"
+else
+    withgcc='--enable-gcc'
+fi
 export CPPFLAGS="-I/usr/tgcware/include"
-export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args="--prefix=$prefix --with-tcl=${prefix}/${_libdir} --with-tclinclude=${prefix}/${_includedir}/tcl-private --with-tkinclude=${prefix}/${_includedir}/tk-private --with-tk=${prefix}/${_libdir} --enable-gcc --enable-shared"
+export LDFLAGS="$NO_RQS -L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 
+configure_args="--prefix=$prefix --with-tcl=${prefix}/${_libdir} --with-tclinclude=${prefix}/${_includedir}/tcl-private --with-tkinclude=${prefix}/${_includedir}/tk-private --with-tk=${prefix}/${_libdir} --enable-shared $with_gcc"
+
+shortroot=1
 topsrcdir=$topdir-${version%.*}
 majorver=5.43
 
@@ -30,6 +41,8 @@ reg prep
 prep()
 {
     generic_prep
+    sleep 1
+    touch configure
 }
 
 reg build
@@ -41,9 +54,10 @@ build()
 reg install
 install()
 {
-    generic_install INSTALL_ROOT
+    generic_install prefix
     setdir ${stagedir}${prefix}/${_libdir}
-    ${LN} -s libexpect${majorver}.so libexpect.so
+    ${__ln} -s libexpect${majorver}.so libexpect.so
+    ${__rm} -f ${_libdir}/expect-${majorver}/*.a
     doc FAQ README HISTORY
 
 }
