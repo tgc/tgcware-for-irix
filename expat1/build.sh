5021ec8fde1f6a9e4a4e5962d65d5a67e6f64387
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-20 10:21:01 +0000

    Rebuild for -no_rqs on Irix 5.3.

--- expat1/build.sh
+++ expat1/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=expat
 version=1.95.8
-pkgver=4
+pkgver=5
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
