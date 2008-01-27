b6875939a61ede1dcad2d27384c47fd599302c97
Tom G. Christensen <tgc@jupiterrise.com>
2008-01-27 18:00:25 +0000

    Update to mpfr 2.3.0 + 4 upstream patches.

--- mpfr/build.sh
+++ mpfr/build.sh
@@ -9,11 +9,14 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=mpfr
-version=2.2.1
+version=2.3.0
 pkgver=1
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=patch01
+patch[1]=patch02
+patch[2]=patch03
+patch[3]=patch04
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -21,7 +24,7 @@ source[0]=$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args="$configure_args --with-gmp=$prefix --enable-shared --disable-dependency-tracking"
+configure_args="$configure_args --with-gmp=$prefix --enable-shared"
 
 reg prep
 prep()
@@ -33,8 +36,13 @@ reg build
 build()
 {
     generic_build
+}
+
+reg check
+check()
+{
     setdir source
-    $MAKE_PROG check
+    ${__make} -k check
 }
 
 reg install
