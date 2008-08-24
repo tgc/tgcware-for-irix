c4abb51379d2a71f308993aaf56f72d6d7cdd155
Tom G. Christensen <tgc@jupiterrise.com>
2008-08-24 15:29:18 +0000

    autoconf 2.62

---
+++ autoconf262/build.sh
@@ -0,0 +1,61 @@
+#!/usr/tgcware/bin/bash
+#
+# This is a generic build.sh script
+# It can be used nearly unmodified with many packages
+# 
+# build.sh helper functions
+. ${BUILDPKG_BASE}/scripts/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=autoconf
+version=2.62
+pkgver=1
+source[0]=ftp://ftp.sunet.se/pub/gnu/autoconf/$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_BASE}/scripts/buildpkg.functions
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc ChangeLog NEWS README COPYING TODO THANKS BUGS AUTHORS
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
