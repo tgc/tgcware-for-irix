dd758c44d2ca5d25c4ee38ec762112dbd2a0ce24
Tom G. Christensen <tgc@jupiterrise.com>
2008-09-16 19:41:32 +0000

    Update to 1.11.3

--- wget/build.sh
+++ wget/build.sh
@@ -9,28 +9,27 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=wget
-version=1.10.2
-pkgver=2
-source[0]=$topdir-$version.tar.gz
+version=1.11.3
+pkgver=1
+source[0]=http://ftp.sunet.se/pub/gnu/wget/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=wget-1.10.2-trio.patch
-patch[1]=wget-1.10.2-m4include.patch
+patch[0]=wget-1.11.3-types.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
+mipspro=1
+export CC=cc
+[ "$_os" = "irix53" ] && NO_RQS="-Wl,-no_rqs"
 export CPPFLAGS="-I/usr/tgcware/include"
-export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+export LDFLAGS="$NO_RQS -L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 ac_overrides="ac_cv_lib_socket_socket=no"
 
 reg prep
 prep()
 {
     generic_prep
-    ${CP} /usr/tgcware/share/aclocal/needtrio.m4 m4
-    autoheader
-    autoconf
 }
 
 reg build
