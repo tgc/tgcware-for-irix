a5ed222c58eccaffb90b41c08c55944255b467ad
Tom G. Christensen <tgc@jupiterrise.com>
2008-11-15 19:31:21 +0000

    Update to 1.8.4

--- cairo/build.sh
+++ cairo/build.sh
@@ -9,12 +9,11 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=cairo
-version=1.2.6
+version=1.8.4
 pkgver=1
-source[0]=$topdir-$version.tar.gz
+source[0]=http://cairographics.org/releases/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]= #cairo-1.0.2-nostdint.patch
-patch[1]=cairo-1.0.2-trio.patch
+patch[0]=cairo-1.8.4-trio.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -22,17 +21,19 @@ patch[1]=cairo-1.0.2-trio.patch
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --enable-xlib-xrender=no --disable-static"
+export CC=cc
+mipspro=1
 
 reg prep
 prep()
 {
     generic_prep
     setdir source
-    aclocal-1.9
+    aclocal-1.10 -I build
+    automake-1.10 -gnu
     autoheader
     autoconf
-    # ARGH! somehow X_LIBS is being translated into /usr/lib on Irix 6.2
-    [ "$_os" = "irix62" ] && sed -i '/-lX11/s|$X_LIBS|-L/usr/lib32|g' configure
 }
 
 reg build
@@ -45,7 +46,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc README NEWS COPYING* TODO
+    doc README NEWS COPYING*
 }
 
 reg pack
