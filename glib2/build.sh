cd75d2351f7e866fbfd947c2de24907c17367dab
Tom G. Christensen <tgc@jupiterrise.com>
2008-11-16 13:34:38 +0000

    Allow building with MIPSpro. Untested on IRIX 5.3.

--- glib2/build.sh
+++ glib2/build.sh
@@ -10,10 +10,13 @@
 # Check the following 4 variables before running the script
 topdir=glib
 version=2.18.2
-pkgver=1
+pkgver=2
 source[0]=http://ftp.gnome.org/pub/gnome/sources/glib/2.18/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=glib-2.18.2-no-pthread.patch
+patch[1]=glib-2.18.2-no-func.patch
+patch[2]=glib-2.18.2-non_constant_initializer_fix.patch
+patch[3]=glib-2.18.2-no-variadic-macros.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -26,6 +29,12 @@ export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 configure_args="$configure_args --with-pcre=system --with-libiconv=gnu"
 # Don't build with pth on 5.3
 [ "$_os" = "irix53" ] && configure_args="$configure_args --with-threads=none"
+# SGI cc has a nasty a habit of ignoring the #error directive
+# For IRIX 5.3 we can use the old GNU ANSI preprocesser as a workaround
+[ "$_os" = "irix53" ] && export CPP="cc -acpp -E"
+# For MIPSpro we use -diag_error 1035 to make #error work properly
+[ "$_os" = "irix62" ] && extra_cc_args="-diag_error 1035"
+export CC="cc $extra_cc_args"
 
 reg prep
 prep()
@@ -47,6 +56,12 @@ install()
     generic_install DESTDIR
     ${__gsed} -i 's|local/perl|tgcware/perl|' ${stagedir}${prefix}/${_bindir}/glib-mkenums
     doc NEWS README COPYING
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg pack
