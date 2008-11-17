0cae1aa327e466ea0dc24d2fba4a65df692a7404
Tom G. Christensen <tgc@jupiterrise.com>
2008-11-17 20:13:11 +0000

    Rebuild with MIPSpro

--- atk/build.sh
+++ atk/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=atk
 version=1.24.0
-pkgver=1
+pkgver=2
 source[0]=http://ftp.acc.umu.se/pub/gnome/sources/atk/1.24/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -21,6 +21,8 @@ source[0]=http://ftp.acc.umu.se/pub/gnome/sources/atk/1.24/$topdir-$version.tar.
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+export CC=cc
+mipspro=1
 
 reg prep
 prep()
