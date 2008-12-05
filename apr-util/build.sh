d12e670e17549a8d6fb9b803bc3630e5bb5ce558
Tom G. Christensen <tgc@jupiterrise.com>
2008-12-05 23:40:22 +0000

    Update to 1.3.4

--- apr-util/build.sh
+++ apr-util/build.sh
@@ -9,9 +9,9 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=apr-util
-version=1.2.10
-pkgver=2
-source[0]=$topdir-$version.tar.bz2
+version=1.3.4
+pkgver=1
+source[0]=http://mirrors.dotsrc.org/apache/apr/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -22,7 +22,7 @@ source[0]=$topdir-$version.tar.bz2
 apuver=1
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args="$configure_args --with-apr=${prefix} --includedir=${prefix}/${_includedir}/apr-${apuver} --with-expat=${prefix} --with-berkeley-db=${prefix} --with-iconv=${prefix} --with-ldap --with-ldap-include=${prefix}/${_includedir} --with-ldap-lib=${prefix}/${_libdir}"
+configure_args="$configure_args --disable-static --with-apr=${prefix} --includedir=${prefix}/${_includedir}/apr-${apuver} --with-expat=${prefix} --with-berkeley-db=${prefix} --with-iconv=${prefix} --with-ldap --with-ldap-include=${prefix}/${_includedir} --with-ldap-lib=${prefix}/${_libdir}"
 
 reg prep
 prep()
@@ -38,6 +38,12 @@ build()
     ${__make} dox
 }
 
+reg check
+check()
+{
+    generic_check
+}
+
 reg install
 install()
 {
@@ -46,6 +52,7 @@ install()
     ${__mkdir} -p ${stagedir}${prefix}/${_vdocdir}/html
     ${__install} -m 644 $srcdir/$topsrcdir/docs/dox/html/* ${stagedir}${prefix}/${_vdocdir}/html
     ${__rm} -f ${stagedir}${prefix}/${_libdir}/aprutil.exp
+    ${__rm} -f ${stagedir}${prefix}/${_libdir}/apr-util-1/*.la
 }
 
 reg pack
