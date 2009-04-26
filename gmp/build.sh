b393e564dfc2dc47e1ec6ac9792917c87a945d2a
Tom G. Christensen <tgc@jupiterrise.com>
2009-04-26 14:47:19 +0000

    Drop __inline patch.

--- gmp/build.sh
+++ gmp/build.sh
@@ -10,14 +10,13 @@
 # Check the following 4 variables before running the script
 topdir=gmp
 version=4.2.4
-pkgver=2
+pkgver=3
 source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=gmp-4.2.4-use-ldflags-during-configure.patch
-patch[1]=gmp-4.2.4-support-inline-with-sgicc.patch
-patch[2]=perfpow.c.diff
-patch[3]=mpf_set_str.c.4.diff
-patch[4]=mpf_eq.diff
+patch[1]=perfpow.c.diff
+patch[2]=mpf_set_str.c.4.diff
+patch[3]=mpf_eq.diff
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -28,9 +27,10 @@ export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 configure_args="--prefix=$prefix --enable-cxx --enable-mpbsd --with-readline=no"
 if [ "$_os" = "irix62" ]; then
     mipspro=1
+    export ABI=n32
     export CC=cc
     export CXX=g++
-    export CXXFLAGS="-O2 -mabi=n32"
+    export CXXFLAGS="-O2 -mabi=$ABI"
 fi
 
 reg prep
