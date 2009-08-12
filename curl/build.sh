9351d5f1c62880ac402577ae062f880415fa326a
Tom G. Christensen <tgc@jupiterrise.com>
2009-08-12 12:55:51 +0000

    Update to 7.19.6

--- curl/build.sh
+++ curl/build.sh
@@ -1,15 +1,29 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.19.0
+version=7.19.6
 pkgver=1
 source[0]=http://curl.haxx.se/download/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -19,9 +33,9 @@ patch[0]=curl-7.17.1-pkgconfig.patch
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
 
 # Global settings
-if [ "$_os" = "irix53" ]; then
-    mipspro=1
-    export CC=cc
+mipspro=1
+export CC=cc
+if irix53; then
     export CPP="cc -acpp -E"
     NO_RQS="-Wl,-no_rqs"
 fi
@@ -58,6 +72,12 @@ install()
 
     # Install libcurl.m4
     ${__install} -D -m 644 docs/libcurl/libcurl.m4 ${stagedir}${prefix}/${_sharedir}/aclocal/libcurl.m4
+
+    # Install curl binary
+    # This is a terrible hack but libtool won't install it because libcurl
+    # was not in it's final destination :(
+    ${__rm} -f ${stagedir}${prefix}/${_bindir}/curl
+    ${__install} -D -m 755 src/.libs/curl ${stagedir}${prefix}/${_bindir}/curl
 }
 
 reg check
