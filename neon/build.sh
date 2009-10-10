139253dc35d64941106ed00d212eed52060ae9d3
Tom G. Christensen <tgc@jupiterrise.com>
2009-10-10 18:31:02 +0000

    Update to 0.29.0

--- neon/build.sh
+++ neon/build.sh
@@ -1,17 +1,31 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=neon
-version=0.26.4
-pkgver=3
-source[0]=$topdir-$version.tar.gz
+version=0.29.0
+pkgver=1
+source[0]=http://webdav.org/neon/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
 
@@ -21,7 +35,7 @@ source[0]=$topdir-$version.tar.gz
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args="$configure_args --with-ssl=gnutls --with-egd=/var/run/egd-pool --disable-static --enable-shared"
+configure_args="$configure_args --with-ssl=gnutls --with-egd=/var/run/egd-pool --disable-static --enable-shared --enable-ld-version-script=no"
 
 reg prep
 prep()
@@ -33,6 +47,12 @@ reg build
 build()
 {
     generic_build
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg install
