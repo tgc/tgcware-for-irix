c110e7a467ad39a4bd1e6b7de88cad2044f01fea
Tom G. Christensen <tgc@jupiterrise.com>
2009-10-10 18:30:08 +0000

    Update to 1.6.5 for IRIX 6.2

--- subversion/build.sh
+++ subversion/build.sh
@@ -1,16 +1,30 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=subversion
-version=1.5.4
-pkgver=2
+version=1.6.5
+pkgver=1
 source[0]=http://subversion.tigris.org/downloads/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
@@ -21,7 +35,7 @@ source[0]=http://subversion.tigris.org/downloads/$topdir-$version.tar.bz2
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args="$configure_args --with-neon=${prefix} --with-apr=${prefix} --with-apr-util=${prefix} --without-jdk"
+configure_args="$configure_args --with-neon=${prefix} --with-apr=${prefix} --with-apr-util=${prefix} --without-jdk --disable-static"
 [ "$_os" = "irix62" ] && ac_overrides="ac_cv_lib_socket_socket=no"
 
 reg prep
@@ -29,7 +43,7 @@ prep()
 {
     generic_prep
     setdir source
-    ${__gsed} -i 's/la-file/libs/g' configure
+    #${__gsed} -i 's/la-file/libs/g' configure
     ${__gsed} -i 's/hardcode_into_libs=yes/hardcode_into_libs=no/g' configure
 }
 
