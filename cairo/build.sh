6ab7f4d4617cb29492be3e29b12ab5fa0713ba01
Tom G. Christensen <tgc@jupiterrise.com>
2009-10-11 11:09:10 +0000

    Update to 1.8.8

--- cairo/build.sh
+++ cairo/build.sh
@@ -1,15 +1,29 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=cairo
-version=1.8.4
+version=1.8.8
 pkgver=1
 source[0]=http://cairographics.org/releases/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -22,16 +36,20 @@ patch[0]=cairo-1.8.4-trio.patch
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
 configure_args="$configure_args --enable-xlib-xrender=no --disable-static"
-export CC=cc
-mipspro=1
 
 reg prep
 prep()
 {
     generic_prep
     setdir source
-    aclocal-1.10 -I build
-    automake-1.10 -gnu
+    # Force linking with -lpthread
+    if irix62; then
+	${__gsed} -i '/^libcairo_la_LIBADD/s@\(.*\)@\1 -lpthread@' src/Makefile.am
+	# Users of libcairo must link with pthread to resolve pthread_* symbols
+	${__gsed} -i '/Libs:/s@\(.*\)@\1 -lpthread@' src/cairo.pc.in
+    fi
+    aclocal -I build
+    automake --gnu
     autoheader
     autoconf
 }
@@ -40,6 +58,12 @@ reg build
 build()
 {
     generic_build
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg install
