1e2b21fbbc735408beed62c4ee231d5cfbce5e01
Tom G. Christensen <tgc@jupiterrise.com>
2009-10-24 10:56:48 +0000

    Update to 2.4.6 for IRIX 5.3

--- python24/build.sh
+++ python24/build.sh
@@ -1,17 +1,31 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=Python
-version=2.4.4
-pkgver=3
-source[0]=$topdir-$version.tar.bz2
+version=2.4.6
+pkgver=1
+source[0]=http://www.python.org/ftp/python/2.4.6/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=Python-2.4.2-tgcware.patch
 patch[1]=Python-2.4.3-irix-shared.patch
@@ -50,8 +64,8 @@ build()
     generic_build
 }
 
-reg test
-test()
+reg check
+check()
 {
     setdir source
     ${__make} test
