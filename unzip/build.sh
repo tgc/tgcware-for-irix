94ff6b9993efccc974714ceec09146c931bcef66
Tom G. Christensen <tgc@jupiterrise.com>
2009-10-25 18:50:27 +0000

    Build for IRIX 5.3. Add copyright and use newstyle tools.

--- unzip/build.sh
+++ unzip/build.sh
@@ -1,8 +1,22 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
@@ -33,8 +47,8 @@ reg build
 build()
 {
     setdir source
-    ${LN} -s unix/Makefile Makefile
-    $MAKE_PROG sgi
+    ${__ln} -s unix/Makefile Makefile
+    ${__make} sgi
 }
 
 reg install
