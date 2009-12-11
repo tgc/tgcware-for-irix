62e60fbf88c68b997ecae00047fc0c6ec41185eb
Tom G. Christensen <tgc@jupiterrise.com>
2009-12-11 23:08:21 +0000

    mpfr 2.4.2 for IRIX 5.3

--- mpfr/build.sh
+++ mpfr/build.sh
@@ -1,19 +1,33 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=mpfr
-version=2.4.1
-pkgver=3
+version=2.4.2
+pkgver=1
 source[0]=http://www.mpfr.org/mpfr-current/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-patch[0]=mpfr-2.4.1-p5.patch
+patch[0]=mpfr-2.4.2-r6264-mips.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -46,7 +60,7 @@ reg install
 install()
 {
     generic_install DESTDIR
-    doc AUTHORS BUGS COPYING* FAQ.html NEWS TODO
+    ${__mv} ${stagedir}${prefix}/${_docdir}/mpfr ${stagedir}${prefix}/${_vdocdir}
 }
 
 reg pack
