70af2521fe1a0a4c8dbf6173b515d32038164436
Tom G. Christensen <tgc@jupiterrise.com>
2009-12-13 13:41:09 +0000

    Add security fixes

--- expat/build.sh
+++ expat/build.sh
@@ -1,8 +1,22 @@
 #!/usr/tgcware/bin/bash
-#
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
+# This is a buildpkg build.sh script
+# Copyright (C) 2003-2009 Tom G. Christensen <tgc@jupiterrise.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Tom G. Christensen <tgc@jupiterrise.com>.
+
 # build.sh helper functions
 . ${BUILDPKG_BASE}/scripts/build.sh.functions
 #
@@ -10,10 +24,11 @@
 # Check the following 4 variables before running the script
 topdir=expat
 version=2.0.1
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=expat-1.95.8-CVE-2009-3560.patch
+patch[1]=expat-1.95.8-CVE-2009-3720.patch
 
 # Source function library
 . ${BUILDPKG_BASE}/scripts/buildpkg.functions
@@ -21,8 +36,6 @@ source[0]=$topdir-$version.tar.gz
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-
-#shortroot=1
 
 reg prep
 prep()
@@ -41,6 +54,8 @@ install()
 {
     generic_install DESTDIR
     ${__rm} -f ${stagedir}${prefix}/${_libdir}/*.a
+    ${__rm} -f examples/*.dsp
+    chmod 644 README COPYING Changes doc/* examples/*
     doc README Changes COPYING doc/reference.html doc/*.png doc/*.css
 }
 
