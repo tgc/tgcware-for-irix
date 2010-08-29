17addf3ad454c5c69d6e6e85e769d4ef02e8acaf
Tom G. Christensen <tgc@fortuna.(none)>
2010-08-29 13:06:01 +0200

    Update to 1.2.5

--- zlib/build.sh
+++ zlib/build.sh
@@ -9,41 +9,52 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=zlib
-version=1.2.3
-pkgver=2
-source[0]=$topdir-$version.tar.bz2
+version=1.2.5
+pkgver=1
+source[0]=http://zlib.net/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=zlib-1.2.5-irixcc.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 # Global settings
 configure_args='--shared --prefix=$prefix' 
-export LDSHARED="gcc -shared -rpath ${prefix}/${_libdir} -Wl,-soname,libz.so.1"
 check_ac=0
 shortroot=1
+mipspro=1
+export CC=cc
+make_check_target=test
 
 reg prep
 prep()
 {
     generic_prep
+
+    setdir source
+    irix53 && ${__gsed} -i 's/^off64_t/no_off64_t/' configure
+    irix53 && ${__gsed} -i '/woff,84/ s/shared/shared -Wl,-no_rqs/' configure
 }
 
 reg build
 build()
 {
     generic_build
-    setdir source
-    ${__make} test
 }
 
 reg install
 install()
 {
     generic_install prefix
-    doc README ChangeLog algorithm.txt minigzip.c example.c FAQ
+    doc README ChangeLog minigzip.c example.c FAQ doc
     ${__mv} ${stagedir}${prefix}/share/${_mandir} ${stagedir}${prefix}
+    ${__rm} -f ${stagedir}${prefix}/${_libdir}/libz.a
+}
+
+reg check
+check()
+{
+    generic_check
 }
 
 reg pack
