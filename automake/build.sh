378a413d7d552218d7bf2d038f62b8616a0d58bc
Tom G. Christensen <tgc@jupiterrise.com>
2010-09-05 09:01:47 +0200

    Update to 1.11.1

--- automake/build.sh
+++ automake/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=automake
-version=1.11
+version=1.11.1
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/automake/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
