49686a5127c0a4cd72c1b8a3d2538b8210f18d81
Tom G. Christensen <tgc@jupiterrise.com>
2011-03-17 13:25:48 +0100

    mpfr 3.0.0pl8 for IRIX 6.2

---
+++ mpfr3/build.sh
@@ -0,0 +1,70 @@
+#!/usr/tgcware/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=mpfr
+version=3.0.0
+pkgver=2
+source[0]=http://www.mpfr.org/mpfr-current/$topdir-$version.tar.bz2
+# If there are no patches, simply comment this
+patch[0]=mpfr-3.0.0-pl8.patch
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I/usr/tgcware/include"
+export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
+configure_args="$configure_args --with-gmp=$prefix --enable-shared"
+CC=gcc
+if irix62; then
+  mipspro=1
+  CC="cc"  
+  export  CFLAGS="-O2 -n32 -OPT:IEEE_NaN_inf=ON"
+fi
+export CC=$CC
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    ${__mv} ${stagedir}${prefix}/${_docdir}/mpfr ${stagedir}${prefix}/${_vdocdir}
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
