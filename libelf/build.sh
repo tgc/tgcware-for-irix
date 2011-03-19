dd67f109f99f7cd4ac4a245d73e35d6eef0d0d2a
Tom G. Christensen <tgc@jupiterrise.com>
2011-03-19 21:21:35 +0100

    libelf 0.8.13 for IRIX 6.2

---
+++ libelf/build.sh
@@ -0,0 +1,62 @@
+#!/usr/tgcware/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libelf
+version=0.8.13
+pkgver=1
+source[0]=http://www.mr511.de/software/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+configure_args="$configure_args --disable-nls --enable-compat=no"
+ac_overrides="ac_cv_header_elf_h=no ac_cv_header_sys_elf_h=no"
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install instroot
+    doc README COPYING.LIB
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
