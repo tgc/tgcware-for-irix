229cc784b3bb79d80e6cfb88eef45b6db4d2f5b0
Tom G. Christensen <tgc@jupiterrise.com>
2011-08-11 17:35:41 +0200

    Bump for rebuild on IRIX 5.3

--- ncurses/build.sh
+++ ncurses/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=ncurses
 version=5.6
-pkgver=1
+pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=ncurses-5.5-destdir.patch
