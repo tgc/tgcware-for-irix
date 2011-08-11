740837107b6a983825697a5d38233441f6ee4508
Tom G. Christensen <tgc@jupiterrise.com>
2011-08-11 11:45:16 +0200

    Bump for rebuild on IRIX 5.3

--- dillo/build.sh
+++ dillo/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=dillo
 version=0.8.6
-pkgver=2
+pkgver=3
 source[0]=$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=dillo-0.8.6-linkorder.patch
