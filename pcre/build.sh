e785072f6cd6ded1fa8299cd3326d9ecd6f8eea4
Tom G. Christensen <tgc@jupiterrise.com>
2011-08-11 10:11:28 +0200

    Bump for rebuild on IRIX 5.3

--- pcre/build.sh
+++ pcre/build.sh
@@ -10,7 +10,7 @@
 # Check the following 4 variables before running the script
 topdir=pcre
 version=7.8
-pkgver=1
+pkgver=2
 source[0]=http://downloads.sourceforge.net/pcre/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 patch[0]=pcre-7.8-no-linker-alias.patch
