8c9efd0484ab043db12f1c20f42d815d22fe2860
Tom G. Christensen <tgc@jupiterrise.com>
2011-08-12 13:46:26 +0200

    Bump for rebuild on IRIX 5.3

--- gnutls/build.sh
+++ gnutls/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=gnutls
 version=2.8.5
-pkgver=1
+pkgver=2
 source[0]=ftp://ftp.gnutls.org/pub/gnutls/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
 #patch[0]=
