d1aaa42773e577fb8fb23bad3bc1d9431cc26a27
Tom G. Christensen <tgc@jupiterrise.com>
2011-08-12 20:47:08 +0200

    Bump for rebuild on IRIX 5.3
    flac 1.2.1 was already released for IRIX 5.3 but the changes had
    never been committed.

--- flac/build.sh
+++ flac/build.sh
@@ -9,34 +9,35 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=flac
-version=1.1.2
+version=1.2.1
 pkgver=2
 source[0]=$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=flac-1.1.2-snprintf.patch
-patch[0]=flac-1.1.2-needstrio.patch
-patch[1]=flac-1.1.2-noxmms.patch
-patch[2]=flac-1.1.2-include.patch
+patch[0]=flac-1.2.1-needstrio.patch
+patch[1]=flac-1.2.1-no-stdint_h.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
+[ "$_os" = "irix53" ] && patch[2]=flac-1.2.1-missing_fseeko_ftello.patch
+
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args='--prefix=$prefix --mandir=${prefix}/${_mandir} --infodir=${prefix}/${_infodir} --disable-rpath'
+configure_args='--prefix=$prefix --mandir=${prefix}/${_mandir} --infodir=${prefix}/${_infodir} --disable-rpath --disable-xmms-plugin --enable-static=no'
 
 reg prep
 prep()
 {
     generic_prep
-    aclocal-1.9
-    automake-1.9
+    libtoolize -f
+    aclocal-1.10 -I m4
+    automake-1.10
     autoheader
     autoconf
     # The libiconv stuff is broken :(
     # This needs GNU sed with inplace editing!
-    $FIND . -name 'Makefile.in' -print|$XARGS -n 1 $GSED -i 's/@LIBICONV@/@LTLIBICONV@/g'
+    ${__find} . -name 'Makefile.in' -print | ${__xargs} -n1 ${__gsed} -i 's/@LIBICONV@/@LTLIBICONV@/g'
 }
 
 reg build
@@ -50,6 +51,11 @@ install()
 {
     generic_install DESTDIR
     doc COPYING* AUTHORS README
+    # Libtool installed its shellscript wrapper instead of the actual binary :(
+    ${__install} -m755 ${srcdir}/${topsrcdir}/src/metaflac/.libs/metaflac ${stagedir}${prefix}/${_bindir}/metaflac
+    ${__install} -m755 ${srcdir}/${topsrcdir}/src/flac/.libs/flac ${stagedir}${prefix}/${_bindir}/flac
+    # This is a bit ugly, but we need to rerun the strip function normally called from generic_install
+    do_strip
 }
 
 reg pack
