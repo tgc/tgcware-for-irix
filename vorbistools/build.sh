395cb707705c02f553733dad3feeb250dc3e3ed0
Tom G. Christensen <tgc@jupiterrise.com>
2011-12-17 16:34:19 +0100

    Update to vorbis-tools 1.2.0

--- vorbistools/build.sh
+++ vorbistools/build.sh
@@ -9,13 +9,12 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=vorbis-tools
-version=1.1.1
-pkgver=4
-source[0]=$topdir-$version.tar.gz
+version=1.2.0
+pkgver=1
+source[0]=http://downloads.xiph.org/releases/vorbis/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 patch[0]=vorbis-tools-1.1.1-needstrio.patch
 patch[1]=vorbis-tools-1.1.1-include.patch
-patch[2]=vorbis-tools-r12202-build-with-modern-curl-versions.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
