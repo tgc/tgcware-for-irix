64621173dbc9b008863b46abbf63a071afae81db
Tom G. Christensen <tgc@jupiterrise.com>
2012-09-07 20:01:20 +0200

    tcpdump: update to 4.3.0

--- tcpdump/build.sh
+++ tcpdump/build.sh
@@ -1,19 +1,17 @@
 #!/usr/tgcware/bin/bash
 #
-# This is a generic build.sh script
-# It can be used nearly unmodified with many packages
-# 
 # build.sh helper functions
 . ${BUILDPKG_SCRIPTS}/build.sh.functions
 #
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=tcpdump
-version=3.9.4
-pkgver=1
-source[0]=$topdir-$version.tar.gz
+version=4.3.0
+pkgver=2
+source[0]=http://www.tcpdump.org/release/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
-patch[0]=tcpdump-3.9.4-no-in6_addr.patch
+patch[0]=tcpdump-4.2.1-timeval.patch
+patch[1]=tcpdump-4.3.0-default-snaplen.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -21,12 +19,16 @@ patch[0]=tcpdump-3.9.4-no-in6_addr.patch
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/include"
 export LDFLAGS="-L/usr/tgcware/lib -Wl,-rpath,/usr/tgcware/lib"
-configure_args="--prefix=$prefix --disable-ipv6"
+configure_args="$configure_args --disable-ipv6"
 
 reg prep
 prep()
 {
     generic_prep
+    setdir source
+    # We need to prevent the detection of inet_ntop and inet_pton since
+    # they're broken on IRIX 6.2
+    ${__gsed} -i 's/inet_ntop(AF/inet_ntop_broken(AF/;s/inet_pton(AF/inet_pton_broken(AF/' configure
 }
 
 reg build
@@ -39,7 +41,8 @@ reg install
 install()
 {
     generic_install DESTDIR
-doc CREDITS LICENSE 
+    doc CREDITS LICENSE
+    ${__rm} ${stagedir}${prefix}/${_sbindir}/tcpdump.*
 }
 
 reg pack
