2915719a714d379a7c7c19ce8b22f8bd3f9e803e
Tom G. Christensen <tgc@jupiterrise.com>
2013-07-09 17:34:30 +0200

    gnuchess: new package, gnuchess 5.05

---
+++ gnuchess/build.sh
@@ -0,0 +1,64 @@
+#!/usr/tgcware/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gnuchess
+version=5.05
+pkgver=1
+source[0]=ftp://ftp.sunet.se/pub/gnu/chess/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -Wl,-rpath,$prefix/lib"
+
+topsrcdir=chess
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc AUTHORS COPYING ChangeLog NEWS INSTALL
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
